2fced4c5c6a4a552dbbacbcbefa423a91f28450b
Tony <tony@pc-tony.com>
2014-09-02 21:42:06 +0000

    Enable the sudoers module and correct the path to the template

--- modules/sudoers/manifests/install/ubuntu/1404.pp
+++ modules/sudoers/manifests/install/ubuntu/1404.pp
@@ -2,6 +2,6 @@ class sudoers::install::ubuntu::1404 (
 ) { 
 
   file {'/etc/sudoers':
-    content => template('sudoers/templates/ubuntu_1404_sudoers.erb');
+    content => template('sudoers/ubuntu_1404_sudoers.erb');
   }
 }
