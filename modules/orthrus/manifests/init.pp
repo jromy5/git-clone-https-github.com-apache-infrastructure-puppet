4e92393376433c52d73fb68f0a0b8e753f646659
Tony <tony@pc-tony.com>
2014-09-05 14:36:21 +0000

    Enable pam changes for orthrus

--- modules/orthrus/manifests/init.pp
+++ modules/orthrus/manifests/init.pp
@@ -4,10 +4,19 @@ class orthrus {
 
   case $asfosname { 
     ubuntu: {
+
       package { 'orthrus': 
         ensure  => present,
         require => apt::source['asf_internal'],
       }
+
+      exec { 'setuid-ortpasswd':
+        command => '/bin/chmod u+s /usr/local/bin/ortpasswd',
+        unless  => '/usr/bin/test -u /usr/local/bin/ortpasswd',
+        onlyif  => '/usr/bin/test -f /usr/local/bin/ortpasswd',
+        require => Package['orthrus'],
+      }
+
     }
     default: {
     }
