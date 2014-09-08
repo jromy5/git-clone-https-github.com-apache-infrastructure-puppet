a7b9b0470ed7e7b7e330ad6642714c9b42aab98c
Tony Stevenson <tony@pc-tony.com>
2014-09-09 00:46:18 +0100

    add near useless documentary link

--- bin/pull.sh
+++ bin/pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "This might be useful reading... http://joncairns.com/2011/10/how-to-use-git-submodules/"
+
 git pull .
 git submodule sync .
 git submodule update --recursive --init .
