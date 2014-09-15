59b069cd3de7e36583a5aa26a5c6d5e9dc611d8b
Tony Stevenson <tony@pc-tony.com>
2014-09-15 21:41:53 +0100

    Fix up syntax

--- modules/asf999/manifests/create_user.pp
+++ modules/asf999/manifests/create_user.pp
@@ -20,8 +20,8 @@ class asf999::create_user (
     }
 
     ssh::user_keys { 'asf999-sshkeys':
-      $keycontent    = $sshkeys,
-      $sshd_keysdir  = $sshd_keysdir,
-      $user          = 'asf999',
+      keycontent    => $sshkeys,
+      sshd_keysdir  => $sshd_keysdir,
+      user          => 'asf999',
     }
 }
