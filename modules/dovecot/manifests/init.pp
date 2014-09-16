6aceb495ce8f2c94ec4e919b4e882636a56ff70d
Chris Lambertus <cml@apache.org>
2014-09-16 15:49:29 -0700

    further refinement to dovecot module parameterized ldapservers

--- modules/dovecot/manifests/init.pp
+++ modules/dovecot/manifests/init.pp
@@ -3,11 +3,6 @@
 class dovecot (
   $dovecot_packages         = [],
   $dovecot_remove_packages  = [],
-  $dovecot_confdir          = '',
-  $dovecot_conffile         = '',
-  $dovecot_conffile_userdb  = '',
-  $dovecot_conffile_passwd  = '',
-  $dovecot_ldapuris         = [],
 ) {
 
   package { $dovecot_packages:
@@ -19,20 +14,12 @@ class dovecot (
   }
   
   file {
-    "${dovecot_confdir}":
+    "/etc/dovecot":
       ensure => directory,
       owner  => 'root',
-      mode   => '0755';
-    "${dovecot_confdir}/${dovecot_conffile}":
-      source => "puppet://modules/dovecot/${dovecot_conffile}",
-      mode   => '0755';
-    "${dovecot_confdir}/${dovecot_conffile_userdb}":
-      source => "puppet://modules/dovecot/${dovecot_conffile_userdb}",
-      mode   => '0755';
-    "${dovecot_confdir}/${dovecot_conffile_passdb}":
-      source => "puppet://modules/dovecot/${dovecot_conffile_passdb}",
-      mode   => '0755';
+      mode   => '0755',
   }
 
   class { "dovecot::install::${asfosname}::${asfosrelease}":
+    ldapservers => $ldapservers,
   }
--- modules/dovecot/manifests/install/ubuntu/1404.pp
+++ modules/dovecot/manifests/install/ubuntu/1404.pp
@@ -1,12 +1,15 @@
 class dovecot::install::ubuntu::1404 (
+  $ldapservers = '',
 )
 {
   file {
     '/etc/dovecot/dovecot.conf':
       content => template('dovecot/dovecot.conf.erb');
     '/etc/dovecot/dovecot-ldap-userdb.ext.conf':
-      content => template('dovecot/dovecot-ldap-userdb.ext.conf.erb');
+      content => template('dovecot/dovecot-ldap-userdb.ext.conf.erb'),
+      require => File['/etc/dovecot/dovecot.conf'];
     '/etc/dovecot/dovecot-ldap-passdb.ext.conf':
-      content => template('dovecot/dovecot-ldap-passdb.ext.conf.erb');
+      content => template('dovecot/dovecot-ldap-passdb.ext.conf.erb'),
+      require => File['/etc/dovecot/dovecot.conf'];
   }
 }
