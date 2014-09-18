cdf7022a577caeb8d1307d99d1e52e069020140c
Tony <tony@pc-tony.com>
2014-09-18 12:43:10 +0000

    Use correct file name for "puppetfile", update ignore for 3rd party, and update pull script

--- bin/pull.sh
+++ bin/pull.sh
@@ -2,10 +2,11 @@
 
 git pull .
 
-if [ ! -f "./puppetfile" ] ; then 
+if [ ! -f "./Puppetfile" ] ; then 
   echo 'It looks like you are not running this script relative to the root of the repo. Until we make it more sensible you need too. Sorry.' 
   exit 1
 fi
 
-export PUPPETFILE_DIR=./3rdparty-modules 
-r10k puppetfile check --verbose --trace && r10k puppetfile install 
+export PUPPETFILE_DIR="3rdparty-modules"
+PUPPETFILE_DIR="3rdparty-modules" r10k puppetfile check --verbose --trace
+PUPPETFILE_DIR="3rdparty-modules" r10k puppetfile install 
