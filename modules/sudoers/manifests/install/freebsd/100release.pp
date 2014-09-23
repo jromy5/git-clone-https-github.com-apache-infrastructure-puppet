6f2ae97447d7a218307053bf79432c4bce87b2a0
Tony Stevenson <tony@pc-tony.com>
2014-09-23 11:21:13 +0100

    use correct path to the template

--- modules/sudoers/manifests/install/freebsd/100release.pp
+++ modules/sudoers/manifests/install/freebsd/100release.pp
@@ -2,6 +2,6 @@ class sudoers::install::freebsd::100release (
 ) { 
 
   file {'/etc/sudoers':
-    content => template('templates/freebsd_100release_sudoers.erb');
+    content => template('sudoers/freebsd_100release_sudoers.erb');
   }
 }
