644123691e5b69093a86d8a75791f086214d4b32
Tony Stevenson <tony@pc-tony.com>
2014-09-23 11:19:48 +0100

    use shortened path to the template

--- modules/sudoers/manifests/install/freebsd/100release.pp
+++ modules/sudoers/manifests/install/freebsd/100release.pp
@@ -2,6 +2,6 @@ class sudoers::install::freebsd::100release (
 ) { 
 
   file {'/etc/sudoers':
-    content => template('/usr/local/etc/puppet/modules/sudoers/templates/freebsd_100release_sudoers.erb');
+    content => template('templates/freebsd_100release_sudoers.erb');
   }
 }
