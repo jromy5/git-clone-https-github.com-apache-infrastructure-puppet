6b292c4b78abed3708a7985981cb0b9593ea35b9
Christopher M. Lambertus <christopher.lambertus@ucdmc.ucdavis.edu>
2014-09-23 18:53:04 -0700

    fixed transposed ext.conf in dovecot template

--- modules/dovecot/manifests/install/ubuntu/1404.pp
+++ modules/dovecot/manifests/install/ubuntu/1404.pp
@@ -5,10 +5,10 @@ class dovecot::install::ubuntu::1404 (
   file {
     '/etc/dovecot/dovecot.conf':
       content => template('dovecot/dovecot.conf.erb');
-    '/etc/dovecot/dovecot-ldap-userdb.ext.conf':
+    '/etc/dovecot/dovecot-ldap-userdb.conf.ext':
       content => template('dovecot/dovecot-ldap-userdb.conf.ext.erb'),
       require => File['/etc/dovecot/dovecot.conf'];
-    '/etc/dovecot/dovecot-ldap-passdb.ext.conf':
+    '/etc/dovecot/dovecot-ldap-passdb.conf.ext':
       content => template('dovecot/dovecot-ldap-passdb.conf.ext.erb'),
       require => File['/etc/dovecot/dovecot.conf'];
   }
