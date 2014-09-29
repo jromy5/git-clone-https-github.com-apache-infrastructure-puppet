23d6033e661324677e19933140106d4cff05495f
Dan Norris <dnorris@apache.org>
2014-09-28 21:41:46 -0400

    Remove hash function for Postgres for now

--- modules/foreman/manifests/postgres.pp
+++ modules/foreman/manifests/postgres.pp
@@ -5,7 +5,7 @@ class foreman::postgres ($password) {
   # TODO Create a real password for this in eyaml
   postgresql::server::db { 'foreman':
     user     => 'foreman',
-    password => postgresql_password('foreman', $password)
+    password => $password
   }
 
 }
