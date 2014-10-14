bd732363a49c170f17f05343cf82a0896edc0c79
Geoffrey Corey <coreyg@apache.org>
2014-10-14 13:56:12 -0700

    fix dep cycle

--- modules/tlp_vhosts/manifests/init.pp
+++ modules/tlp_vhosts/manifests/init.pp
@@ -15,8 +15,8 @@ class tlp_vhosts (
     }
 
     anchor { 'tlp_vhosts::begin': } ->
-    class { '::tlp_vhosts::compat': } ->
     class { '::tlp_vhosts::base': } ->
+    class { '::tlp_vhosts::compat': } ->
     class { '::tlp_vhosts::config': } ->
     anchor { 'tlp_vhosts::end': }
 }
