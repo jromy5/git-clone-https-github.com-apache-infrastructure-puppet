3964afea608db46ea8a8815de8d24405c14dfaa3
Tony Stevenson <tony@pc-tony.com>
2014-10-21 21:00:38 +0100

    Add a new clamav module

---
+++ modules/clamav/manifests/init.pp
@@ -0,0 +1,30 @@
+#/etc/puppet/modules/clamav/manifests/init.pp
+
+class clamav (
+  $packages   = ['clamav', 'clamsmtp'],
+
+){
+
+  package { $packages: 
+    ensure   =>  installed,
+  }
+
+  service {
+    'clamav-daemon': 
+      ensure  => running,
+      require => Package[ ['clamav'], ['clamsmtp'] ];
+    'clamav-freshclam':
+      ensure  => running,
+      require => Package[ ['clamav'], ['clamsmtp'] ];
+    'clamavsmtp':
+      ensure  => running,
+      require => Package[ ['clamav'], ['clamsmtp'] ];
+  }
+
+  file { 
+    '/etc/clamsmtpd.conf':
+      content => template('clamav/clamsmtpd.conf.erb'),
+      notify  => Service[ ['clamav-daemon'], ['clamav-freshclam'], ['clamavsmtp'],
+      require => Package[ ['clamav'], ['clamsmtp'] ];
+  }
+}
