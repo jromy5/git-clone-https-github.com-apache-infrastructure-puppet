7c61aad90e97cf402c8314ff0fb36e392be9a17d
Tony Stevenson <tony@pc-tony.com>
2014-10-21 21:52:44 +0100

    bump the path

--- modules/pflogsumm/files/create-pflogsumm.output.sh
+++ modules/pflogsumm/files/create-pflogsumm.output.sh
@@ -3,7 +3,7 @@
 DATE_DAY=`date +%d`
 DATE_MONTH=`date +%m`
 DATE_YEAR=`date +%Y`
-FILEPATH="/var/www/pflogsumm/${DATE_YEAR}/${DATE_MONTH}"
+FILEPATH="/var/www/html/pflogsumm/${DATE_YEAR}/${DATE_MONTH}"
 FILENAME="${DATE_YEAR}${DATE_MONTH}${DATE_DAY}.txt"
 
 if [ ! -d "${FILEPATH}" ]; then
