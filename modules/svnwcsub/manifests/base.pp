6c604019bdfa8eb1b49ac23c9ca02d1290cbbd03
Geoffrey Corey <coreyg@apache.org>
2014-10-21 19:09:38 -0700

    Remove svnwcsub.conf from puppet module in favor of svn (for now) Remove uneeded permissions change

--- modules/svnwcsub/manifests/base.pp
+++ modules/svnwcsub/manifests/base.pp
@@ -20,9 +20,9 @@ class svnwcsub::base inherits svnwcsub {
         gid => $gid,
     }
 
-    exec { 'apache_perms':
-        path => ['/bin/'],
-        command => 'chown svnwc:www-data /var/www ; chmod 2755 /var/www',
-        require => User['svnwc'],
-    }
+#    exec { 'apache_perms':
+#        path => ['/bin/'],
+#        command => 'chown svnwc:www-data /var/www ; chmod 2755 /var/www',
+#        require => User['svnwc'],
+#    }
 }
