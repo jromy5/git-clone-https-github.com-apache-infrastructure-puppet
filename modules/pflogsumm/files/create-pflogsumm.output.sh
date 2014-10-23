883aa185317dca90cfe63eb07ea7cd063ea98fec
Tony Stevenson <tony@pc-tony.com>
2014-10-23 09:17:26 +0100

    change to match base path in the apache install

--- modules/pflogsumm/files/create-pflogsumm.output.sh
+++ modules/pflogsumm/files/create-pflogsumm.output.sh
@@ -3,7 +3,7 @@
 DATE_DAY=`/bin/date +%d`
 DATE_MONTH=`/bin/date +%m`
 DATE_YEAR=`/bin/date +%Y`
-FILEPATH="/var/www/html/pflogsumm/${DATE_YEAR}/${DATE_MONTH}"
+FILEPATH="/var/www/pflogsumm/${DATE_YEAR}/${DATE_MONTH}"
 FILENAME="${DATE_YEAR}${DATE_MONTH}${DATE_DAY}.txt"
 
 if [ ! -d "${FILEPATH}" ]; then
