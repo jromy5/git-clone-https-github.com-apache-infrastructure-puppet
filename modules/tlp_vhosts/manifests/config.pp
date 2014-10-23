38e71ae3c459913d347f37b202e61a2c1d640d19
Geoffrey Corey <coreyg@apache.org>
2014-10-23 13:32:15 -0700

    Enable headers mod

--- modules/tlp_vhosts/manifests/config.pp
+++ modules/tlp_vhosts/manifests/config.pp
@@ -9,6 +9,7 @@ class tlp_vhosts::config inherits tlp_vhosts {
     apache::mod { 'allowmethods': }
     apache::mod { 'cgi': }
     apache::mod { 'cgid': }
+    apache::mod { 'headers': }
     class { 'apache::mod::status':
         allow_from => ['all'],
         apache_version => '2.3', # Force module to use Allow from syntax and actually allow anyone to check
