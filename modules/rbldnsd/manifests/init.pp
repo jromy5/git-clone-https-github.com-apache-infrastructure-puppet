fb5f88e09381e0d2be380bc1ce8674c662b880db
Tony Stevenson <tony@pc-tony.com>
2014-10-23 10:00:08 +0100

    add service notification

--- modules/rbldnsd/manifests/init.pp
+++ modules/rbldnsd/manifests/init.pp
@@ -32,10 +32,15 @@ class rbldnsd (
     '/etc/default/rbldnsd':
       owner    => 'root',
       mode     => '0750',
-      content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net dnsbl.apache.org:ip4set:dnsbl.apache.org"';
+      content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net dnsbl.apache.org:ip4set:dnsbl.apache.org"',
+      notify   => Service['rbldnsd'];
     '/etc/rbldnsd':
       owner    => 'root',
-      mode     => '0750';
+      mode     => '0750',
+      notify   => Service['rbldnsd'];
   }
 
+  service { 'rbldnsd':
+    ensure     => 'running',
+  }
 }
