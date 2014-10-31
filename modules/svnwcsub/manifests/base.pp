808ecd3ae1d221e882a754e292c2bb5e0ee16549
David Nalley <david@gnsa.us>
2014-10-31 15:10:13 -0400

    more delinting

--- modules/svnwcsub/manifests/base.pp
+++ modules/svnwcsub/manifests/base.pp
@@ -2,22 +2,22 @@
 class svnwcsub::base inherits svnwcsub {
 
     user { 'svnwc':
-        name => 'svnwc',
-        ensure => present,
-        home => '/home/svnwc',
-        shell => '/bin/bash',
-        uid => $uid,
-        gid => 'svnwc',
-        groups => ['apmirror'],
+        name       => 'svnwc',
+        ensure     => present,
+        home       => '/home/svnwc',
+        shell      => '/bin/bash',
+        uid        => $uid,
+        gid        => 'svnwc',
+        groups     => ['apmirror'],
         managehome => true,
-        require => Group['svnwc'],
+        require    => Group['svnwc'],
     }
 
 
     group { 'svnwc':
-        name => 'svnwc',
+        name   => 'svnwc',
         ensure => present,
-        gid => $gid,
+        gid    => $gid,
     }
 
 #    exec { 'apache_perms':
