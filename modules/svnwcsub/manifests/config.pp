826080915696606ff3c6901766ce5a6fa273bf84
David Nalley <david@gnsa.us>
2014-10-31 15:05:30 -0400

    delinting a bit

--- modules/svnwcsub/manifests/config.pp
+++ modules/svnwcsub/manifests/config.pp
@@ -3,37 +3,37 @@ class svnwcsub::config inherits svnwcsub {
 
     file { '/var/log/svnwcsub':
         ensure => directory,
-        mode => 0755,
-        owner => 'svnwc',
-        group => 'svnwc',
+        mode   => 0755,
+        owner  => 'svnwc',
+        group  => 'svnwc',
     }
 
     file { '/var/run/svnwcsub':
         ensure => directory,
-        mode => 0755,
-        owner => 'svnwc',
-        group => 'svnwc',
+        mode   => 0755,
+        owner  => 'svnwc',
+        group  => 'svnwc',
     }
 
     file { '/etc/init.d/svnwcsub':
-        mode => 0755,
-        owner => 'root',
-        group => 'root',
+        mode   => 0755,
+        owner  => 'root',
+        group  => 'root',
         source => 'puppet:///modules/svnwcsub/svnwcsub.debian',
     }
 
     file { '/home/svnwc/svnwcsub-hook':
-        mode => 0755,
-        owner => 'svnwc',
-        group => 'svnwc',
+        mode   => 0755,
+        owner  => 'svnwc',
+        group  => 'svnwc',
         source => 'puppet:///modules/svnwcsub/svnwcsub-hook',
     }
 
     file { '/etc/svnwcsub.conf':
         notify => Service["svnwcsub"],
-        mode => 0644,
-        owner => 'root',
-        group => 'root',
+        mode   => 0644,
+        owner  => 'root',
+        group  => 'root',
         source => 'puppet:///modules/svnwcsub/svnwcsub.conf',
     }
 
