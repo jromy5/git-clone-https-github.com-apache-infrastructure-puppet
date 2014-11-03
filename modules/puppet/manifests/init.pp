444e8b6cb7d64e5489e4a3e2f16fbd0ecd4f7500
David Nalley <david@gnsa.us>
2014-11-03 17:59:22 -0500

    adding some puppet management stuff

---
+++ modules/puppet/manifests/init.pp
@@ -0,0 +1,27 @@
+class puppet {
+
+  package { 'puppet':
+    ensure  => '3.6.2-1puppetlabs1',
+    require => apt::source['asf_internal'],
+  }
+
+  service { 'puppet':
+    require => package['puppet'],
+    hasstatus => true,
+    hasrestart => true,
+    enable => true,
+    ensure => running,
+  }
+
+ file { "/etc/puppet/puppet.conf" :
+   ensure  => 'present',
+   require => Package["puppet"],
+   notify  => Service["puppet"],
+   owner   => "root",
+   group   => "puppet",
+   mode    => 755,
+   source  => [ 
+     "puppet:///modules/puppet/puppet.$hostname.conf",
+     "puppet:///modules/puppet/puppet.conf",
+     ]
+  }
