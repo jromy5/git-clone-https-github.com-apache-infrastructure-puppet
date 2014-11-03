d372d34e36aa0bdaa590746b80d4af2ebe20e565
Tony Stevenson <tony@pc-tony.com>
2014-11-03 15:25:33 +0000

    Have the client fetch the content that the puppetmaster fetches via cron

---
+++ modules/rbldnsd/manifests/client/setup.pp
@@ -0,0 +1,32 @@
+#/etc/puppet/modules/rbldnsd/manifests/init.pp
+
+class rbldnsd (
+  $packages   = ['rbldnsd'],
+
+){
+
+  package { $packages: 
+    ensure   =>  installed,
+  }
+
+
+  file { 
+    '/etc/default/rbldnsd':
+      owner    => 'root',
+      mode     => '0750',
+      content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net dnsbl.apache.org:ip4set:dnsbl.apache.org"',
+      notify   => Service['rbldnsd'];
+    '/etc/rbldnsd':
+      ensure   => 'directory',
+      owner    => 'root',
+      group    => 'rbldns',
+      mode     => '0770',
+      recurse  => 'true',
+      source   => 'puppet:///modules/rbldnsd/rbldnsd/',
+      notify   => Service['rbldnsd'];
+  }
+
+  service { 'rbldnsd':
+    ensure     => 'running',
+  }
+}
