e017f1c4dfce76740611933d06f059124cb55b0a
Tony Stevenson <tony@pc-tony.com>
2014-11-03 16:15:26 +0000

    Drop the old zone

--- modules/rbldnsd/manifests/client/setup.pp
+++ modules/rbldnsd/manifests/client/setup.pp
@@ -14,7 +14,7 @@ class rbldnsd::client::setup (
     '/etc/default/rbldnsd':
       owner    => 'root',
       mode     => '0750',
-      content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net dnsbl.apache.org:ip4set:dnsbl.apache.org"',
+      content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net"',
       notify   => Service['rbldnsd'];
 
     '/etc/rbldnsd':
