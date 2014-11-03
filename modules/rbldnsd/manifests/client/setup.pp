84af80268d79a77b6d614fbdc9b7796829e504ed
Tony Stevenson <tony@pc-tony.com>
2014-11-03 15:32:01 +0000

    Create a dependency on the folder so that the service can start

--- modules/rbldnsd/manifests/client/setup.pp
+++ modules/rbldnsd/manifests/client/setup.pp
@@ -16,6 +16,7 @@ class rbldnsd (
       mode     => '0750',
       content  => 'RBLDNSD="dsbl -b 127.0.0.1 -r /etc/rbldnsd -t 21600 -c 30 sbl-xbl-pbl.spamhaus.org:ip4set:sbl/sbl sbl-xbl-pbl.spamhaus.org:ip4set:xbl/xbl sbl-xbl-pbl.spamhaus.org:ip4set:pbl/pbl dul.dnsbl.sorbs.net:ip4set:sorbs/dul.dnsbl.sorbs.net dul.dnsbl.sorbs.net:generic:sorbs/generic.sorbs.net dnsbl.apache.org:ip4set:dnsbl.apache.org"',
       notify   => Service['rbldnsd'];
+
     '/etc/rbldnsd':
       ensure   => 'directory',
       owner    => 'root',
@@ -28,5 +29,6 @@ class rbldnsd (
 
   service { 'rbldnsd':
     ensure     => 'running',
+    require    => File['/etc/rbldnsd'],
   }
 }
