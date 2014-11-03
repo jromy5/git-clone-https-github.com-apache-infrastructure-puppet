8adbe4b1c9892bc21763441462f965ca8e8bf3d2
Tony Stevenson <tony@pc-tony.com>
2014-11-03 13:42:13 +0000

    Add a concept of a master node, that will distribute the updates to clients

---
+++ modules/rbldnsd/manifests/master/setup.pp
@@ -0,0 +1,41 @@
+#/etc/puppet/modules/rbldnsd/manifests/master/setup.pp
+
+class rbldnsd::master::setup (
+
+){
+
+  cron { 
+    'rbldnsd-sorbs-datafeed':
+      ensure   => present,
+      require  => File['/var/rbldnsd'],
+      command  => '/usr/bin/rsync -az rsync.nl.sorbs.net::rbldnszones/ /var/rbldnsd/sorbs/ > /dev/null',
+      hour     => '*',
+      minute   => '15';
+    'rbldnsd-spamhaus-sbl-datafeed':
+      ensure   => present,
+      require  => File['/var/rbldnsd'],
+      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/sbl /var/rbldnsd/sbl/ > /dev/null',
+      hour     => '*',
+      minute   => '20';
+    'rbldnsd-spamhaus-xbl-datafeed':
+      ensure   => present,
+      require  => File['/var/rbldnsd'],
+      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/xbl /var/rbldnsd/xbl/ > /dev/null',
+      hour     => '*',
+      minute   => '25';
+    'rbldnsd-spamhaus-pbl-datafeed':
+      ensure   => present,
+      require  => File['/var/rbldnsd'],
+      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/pbl /var/rbldnsd/pbl/ > /dev/null',
+      hour     => '*',
+      minute   => '30',
+  }
+
+    '/var/rbldnsd':
+      ensure   => 'directory',
+      owner    => 'root',
+      group    => 'rbldns',
+      mode     => '0770',
+  }
+
+}
