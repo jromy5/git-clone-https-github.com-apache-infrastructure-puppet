3ed60484f44f993a7efcc837b9f254f7f366ae53
Tony <tony@pc-tony.com>
2014-11-03 15:53:28 +0000

    Pull in the data directly into the puppet module path, the symlink option makes this a monumental PITA. Then add a .gitignore so folsk dont accidentally add it to the wc

--- modules/rbldnsd/manifests/master/setup.pp
+++ modules/rbldnsd/manifests/master/setup.pp
@@ -3,45 +3,38 @@
 class rbldnsd::master::setup (
 
   $RBLDISTDIR  =  '/etc/puppet/modules/rbldnsd/files/rbldnsd',
-  $RBLSRCDIR   =  '/var/rbldnsd'
 ){
 
   cron { 
     'rbldnsd-sorbs-datafeed':
       ensure   => present,
-      require  => File['/var/rbldnsd'],
-      command  => '/usr/bin/rsync -az rsync.nl.sorbs.net::rbldnszones/ /var/rbldnsd/sorbs/ > /dev/null',
+      require  => File["${RBLDISTDIR}"],
+      command  => "/usr/bin/rsync -az rsync.nl.sorbs.net::rbldnszones/ ${RBLDISTDIR}/sorbs/ > /dev/null",
       hour     => '*',
       minute   => '15';
     'rbldnsd-spamhaus-sbl-datafeed':
       ensure   => present,
-      require  => File['/var/rbldnsd'],
-      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/sbl /var/rbldnsd/sbl/ > /dev/null',
+      require  => File["${RBLDISTDIR}"],
+      command  => "/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/sbl ${RBLDISTDIR}/sbl/ > /dev/null",
       hour     => '*',
       minute   => '20';
     'rbldnsd-spamhaus-xbl-datafeed':
       ensure   => present,
-      require  => File['/var/rbldnsd'],
-      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/xbl /var/rbldnsd/xbl/ > /dev/null',
+      require  => File["${RBLDISTDIR}"],
+      command  => "/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/xbl ${RBLDISTDIR}/xbl/ > /dev/null",
       hour     => '*',
       minute   => '25';
     'rbldnsd-spamhaus-pbl-datafeed':
       ensure   => present,
-      require  => File['/var/rbldnsd'],
-      command  => '/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/pbl /var/rbldnsd/pbl/ > /dev/null',
+      require  => File["${RBLDISTDIR}"],
+      command  => "/usr/bin/rsync -az rsync://rsync1.spamhaus.org/rbldnsd/pbl ${RBLDISTDIR}/pbl/ > /dev/null",
       hour     => '*',
       minute   => '30',
   }
 
   file {
-    '/var/rbldnsd':
-      ensure   => 'directory',
-      owner    => 'root',
-      group    => 'root',
-      mode     => '0770';
     "${RBLDISTDIR}":
-      ensure   => link,
-      target   => "${RBLSRCDIR}",
+      ensure   => 'directory',
       owner    => 'root',
       group    => 'root',
       mode     => '0770';
