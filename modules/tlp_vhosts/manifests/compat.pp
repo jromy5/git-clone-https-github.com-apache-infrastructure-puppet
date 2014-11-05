fea22913403a0b2b544c44b85efdaa84b1e4491b
Geoffrey Corey <coreyg@apache.org>
2014-11-05 09:37:32 -0800

    typo

--- modules/tlp_vhosts/manifests/compat.pp
+++ modules/tlp_vhosts/manifests/compat.pp
@@ -5,60 +5,60 @@ class tlp_vhosts::compat inherits tlp_vhosts {
     # to appease the hardcoded binary locations in the
     # scripts
     file { '/usr/local/bin/rsync':
-        ensure => 'link',
-        target => '/usr/bin/rsync',
+        ensure  => 'link',
+        target  => '/usr/bin/rsync',
     }
 
     file { '/usr/local/bin/wget':
-        ensure => 'link',
-        target => '/usr/bin/wget',
+        ensure  => 'link',
+        target  => '/usr/bin/wget',
     }
 
     file { '/usr/local/bin/svn':
-        ensure => 'link',
-        target => '/usr/bin/svn',
+        ensure  => 'link',
+        target  => '/usr/bin/svn',
     }
 
     file { '/www':
-        ensure => 'link',
-        target => '/var/www',
+        ensure  => 'link',
+        target  => '/var/www',
     }
     
     file { '/x1':
-        ensure => 'link',
-        target => '/var',
+        ensure  => 'link',
+        target  => '/var',
     }
 
     file { '/usr/local/bin/python2.7':
-        ensure => 'link',
-        target => '/usr/bin/python2.7',
+        ensure  => 'link',
+        target  => '/usr/bin/python2.7',
     }
 
     $apache_org_dirs = ['/var/www', '/var/www/www.apache.org', '/var/www/www.apache.org/dist', '/var/www/www.apache.org/dist/zzz']
 
     file { $apache_org_dirs:
-        ensure => 'directory',
-        owner => 'svnwc',
-        group => 'apmirror',
-        mode => '2775',
+        ensure  => 'directory',
+        owner   => 'svnwc',
+        group   => 'apmirror',
+        mode    => '2775',
     }
 
     $dist_files = ['.htaccess', '.message', 'README.html', 'HEADER.html', 'favicon.ico']
 
     file { $dist_files:
-        ensure => present,
-        path => '/var/www/www.apache.org/dist/HEADER.html',
-        mode => 0644,
-        owner => 'svnwc',
-        group => 'svnwc',
-        source => "puppet:///modules/tlp_vhosts/$dist_files",
+        ensure  => present,
+        path    => "/var/www/www.apache.org/dist/$dist_files",
+        mode    => 0644,
+        owner   => 'svnwc',
+        group   => 'svnwc',
+        source  => "puppet:///modules/tlp_vhosts/$dist_files",
         require => File['/var/www/www.apache.org/dist'],
     }
 
     $packages = ['python-geoip', 'swish-e', 'python-flup']
 
     package { $packages:
-        ensure => 'latest',
+        ensure  => 'latest',
     }
 
 }
