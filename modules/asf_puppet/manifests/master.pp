43cf648af6d75158f7f1ac204b7a5ec8f33df104
David Nalley <david@gnsa.us>
2014-11-12 15:15:53 -0500

    first commit for foreman report support

--- modules/asf_puppet/manifests/master.pp
+++ modules/asf_puppet/manifests/master.pp
@@ -6,5 +6,26 @@ class puppet::master {
     minute  => '*/5',
   }
 
-}
+  package { 'puppetmaster':
+    ensure  => '3.6.2-1puppetlabs1',
+    require => apt::source['puppetlabs', 'puppetdeps'],
+  }
 
+  service { 'puppetmaster':
+    require    => package['puppetmaster']
+    hasstatus  => true,
+    hasrestart => true,
+    ensure     => running,
+  }
+
+  file { "/usr/lib/ruby/vendor_ruby/puppet/reports/foreman.rb":
+   ensure  => 'present',
+   require => Package["puppet"],
+   notify  => Service["puppetmaster"],
+   owner   => "root",
+   group   => "puppet",
+   mode    => 755,
+   source  => "puppet:///modules/asf_puppet/foreman.rb"
+  }
+
+}
