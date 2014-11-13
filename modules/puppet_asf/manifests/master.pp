161d191d352db94307ee82d108868640e3426720
Dan Norris <dnorris@apache.org>
2014-11-12 22:54:15 -0500

    Add rack directories for Puppet master

--- modules/puppet_asf/manifests/master.pp
+++ modules/puppet_asf/manifests/master.pp
@@ -1,5 +1,5 @@
 class puppet_asf::master {
-  include puppet_asf
+
   cron { 'updatepuppet':
     command => 'cd /etc/puppet; /usr/bin/git pull',
     user    => root,
@@ -11,14 +11,21 @@ class puppet_asf::master {
     require => apt::source['puppetlabs', 'puppetdeps'],
   }
 
+  service { 'puppetmaster':
+    ensure     => running,
+    require    => package['puppetmaster'],
+    hasstatus  => true,
+    hasrestart => true,
+  }
 
   file { '/usr/lib/ruby/vendor_ruby/puppet/reports/foreman.rb':
     ensure  => 'present',
     require => Package['puppet'],
+    notify  => Service['puppetmaster'],
     owner   => 'root',
     group   => 'puppet',
     mode    => '0755',
-    source  => 'puppet:///modules/puppet_asf/foreman.rb'
+    source  => 'puppet:///modules/asf_puppet/foreman.rb'
   }
 
   file { '/etc/puppet/foreman.yaml':
@@ -29,4 +36,34 @@ class puppet_asf::master {
     mode    => '644',
     source  => 'puppet:///modules/puppet_asf/foreman.yaml',
   }
+
+  file { 'puppetmaster':
+    require => Package['puppet'],
+    path    => '/usr/share/puppet/rack/puppetmasterd',
+    ensure  => directory,
+    owner   => 'puppet',
+    group   => 'puppet',
+  }
+
+  file { '/usr/share/puppet/rack/puppetmasterd/config.ru':
+    require => File['puppetmaster'],
+    ensure  => present,
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0644',
+  }
+
+  $puppet_dirs = [
+    '/usr/share/puppet/rack/puppetmasterd/public',
+    '/usr/share/puppet/rack/puppetmasterd/tmp',
+  ]
+
+  file  { $puppet_dirs:
+    require => File['puppetmaster'],
+    ensure  => directory,
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0755',
+  }
+
 }
