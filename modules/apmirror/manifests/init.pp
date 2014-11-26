f502be1fccfe22fbc999295d498421c7e62dd570
Geoffrey Corey <coreyg@apache.org>
2014-11-26 08:59:32 -0800

    Parameterize apmirror module better

--- modules/apmirror/manifests/init.pp
+++ modules/apmirror/manifests/init.pp
@@ -1,42 +1,48 @@
 
 class apmirror (
-    $uid            = 508,
-    $gid            = 508,
+  $uid            = 508,
+  $gid            = 508,
+  $groupname      = 'apmirror',
+  $groups         = [''],
+  $service_ensure = 'running',
+  $service_name   = 'svnwc',
+  $shell          = '/bin/bash',
+  $username       = 'apmirror',
 ){
 
-   user { 'apmirror':
-        name        => 'apmirror',
+   user { "${username}":
+        name        => "${username}",
         ensure      => present,
-        home        => '/home/apmirror',
-        shell       => '/bin/bash',
-        uid         => $uid,
-        gid         => 'apmirror',
-        groups      => ['svnwc'],
+        home        => "/home/${username}",
+        shell       => "${shell}",
+        uid         => "${uid}",
+        gid         => "${groupname}",
+        groups      => "${groups}",
         managehome  => true,
-        require     => [ Group['apmirror'], Group['svnwc'] ],
+        require     => [ Group["${groupname}"], Group['svnwc'] ],
     }
 
-    group { 'apmirror':
-        name        => 'apmirror',
+    group { "${groupname}":
+        name        => "${groupname}",
         ensure      => present,
-        gid         => $gid,
+        gid         => "${gid}",
     }
 
     exec { 'apmirror-co':
         command => 'svn co http://svn.apache.org/repos/asf/infrastructure/site-tools/trunk/mirrors/',
         path    => "/usr/bin/:/bin/",
-        cwd     => '/home/apmirror',
-        user    => 'apmirror',
-        group   => 'apmirror',
-        creates => '/home/apmirror/mirrors',
-        require => [ Package['subversion'], User['apmirror'] ],
+        cwd     => "/home/${username}",
+        user    => "${username}",
+        group   => "${groupname}",
+        creates => "/home/${username}/mirrors",
+        require => [ Package['subversion'], User["${username}"] ],
     }
 
     cron { 'apmirror':
-        command     => '/home/apmirror/mirrors/runmirmon.sh',
+        command     => "/home/${username}/mirrors/runmirmon.sh",
         minute      => '19',
-        user        => 'apmirror',
-        require     => User['apmirror'],
+        user        => "${username}",
+        require     => User["${username}"],
     }
 
     # Create symlinks to where the apmirror scripts think the binaries live
