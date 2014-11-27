5b9d442d80c296761db657cd9b1469472e8fd453
Tony Stevenson <tony@pc-tony.com>
2014-11-27 16:00:10 +0000

    Typo fixing.

--- modules/wiki_asf/manifests/init.pp
+++ modules/wiki_asf/manifests/init.pp
@@ -2,7 +2,7 @@
 
 class wiki_asf {
 
-  apache::custom_config: { 'wiki.apache.org':
+  apache::custom_config { 'wiki.apache.org':
     source    => "puppet:///modules/wiki_asf/wiki.apache.org.conf",
     priority  => '10',
     ensure    => present,
