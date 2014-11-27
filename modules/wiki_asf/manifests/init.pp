843457867109fbeebb517e47cc8f59b8dd09d831
Tony Stevenson <tony@pc-tony.com>
2014-11-27 15:50:17 +0000

    Add a new module to manage the moin wiki service as is, via apache::custom_config for now

---
+++ modules/wiki_asf/manifests/init.pp
@@ -0,0 +1,12 @@
+#/etc/puppet/modules/wiki_asf/manifests/init.pp
+
+class wiki_asf {
+
+  apache::custom_config: { 'wiki.apache.org':
+    source    => "puppet:///modules/wiki_asf/wiki.apache.org.conf",
+    priority  => '10',
+    ensure    => present,
+    require   => Class['apache'],
+  }
+
+}
