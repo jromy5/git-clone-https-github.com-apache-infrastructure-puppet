b48e56507fedee67604de31a4c1ea7cefc3bdc54
gmcdonald <gmcdonald@apache.org>
2014-12-04 15:33:53 +0000

    try svn.au

--- modules/svnpubsub/manifests/common.pp
+++ modules/svnpubsub/manifests/common.pp
@@ -2,7 +2,7 @@
 class svnpubsub::common {
 
     exec { 'pubsub-co':
-        command => 'svn co http://svn.apache.org/repos/asf/subversion/trunk/tools/server-side/svnpubsub',
+        command => 'svn co http://svn.eu.apache.org/repos/asf/subversion/trunk/tools/server-side/svnpubsub',
         path    => "/usr/bin/:/bin/",
         cwd     => '/opt',
         creates => '/opt/svnpubsub',
