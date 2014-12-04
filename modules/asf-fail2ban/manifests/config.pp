2de5d022407cb1779a46d886f84d2ae6531d4e69
Chris Lambertus <cml@apache.org>
2014-12-03 21:33:20 -0800

    cleaning up include location

--- modules/asf-fail2ban/manifests/config.pp
+++ modules/asf-fail2ban/manifests/config.pp
@@ -1,9 +1,11 @@
-include fail2ban
 
 class asf-fail2ban::config (
   $filters,
   $jails,
 ) {
+
+  include fail2ban
+
   create_resources(fail2ban::jail, $jails)
   create_resources(fail2ban::filter, $filters)
 }
