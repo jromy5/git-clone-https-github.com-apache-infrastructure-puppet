44e072403775476fbe2b3956520a7b85a6cf3532
Tony Stevenson <tony@pc-tony.com>
2014-12-10 18:46:14 +0000

    Lets start adding more git stuff

--- modules/gitserver_asf/manifests/init.pp
+++ modules/gitserver_asf/manifests/init.pp
@@ -1,4 +1,22 @@
 #/etc/puppet/modules/gitserver_asf/manifests/init.pp
 
-class gitserver_asf {
+class gitserver_asf (
+
+$packages = []
+
+) {
+
+package { $packages: 
+  ensure  => installed,
+}
+
+file {
+  '/etc/gitweb':
+    ensure   => directory,
+    require  => Package["$packages"],
+    owner    => 'root',
+    group    => 'www-data',
+    mode     => '0750';
+  }
+
 }
