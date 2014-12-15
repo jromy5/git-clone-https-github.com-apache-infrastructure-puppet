ab3ac34309f6c3de2e8fc0e882b6093f6c201f90
Geoffrey Corey <coreyg@apache.org>
2014-12-15 13:48:13 -0800

    Slight typo

--- modules/fail2ban_asf/manifests/config.pp
+++ modules/fail2ban_asf/manifests/config.pp
@@ -4,9 +4,9 @@ class fail2ban_asf::config (
 
   include fail2ban
 
-  $jails = hiera_hash('fail2ban::config::jails', {})
+  $jails = hiera_hash('fail2ban_asf::config::jails', {})
   create_resources(fail2ban::jail, $jails)
-  $filters = hiera_hash('fail2ban::config::filters', {})
+  $filters = hiera_hash('fail2ban_asf::config::filters', {})
   create_resources(fail2ban::filter, $filters)
 }
 
