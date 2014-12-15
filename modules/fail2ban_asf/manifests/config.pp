63eab0f729afe7bffeca2badd9af9465320fb144
Geoffrey Corey <coreyg@apache.org>
2014-12-15 11:28:43 -0800

    Do not require a filter or jail to be specififed

--- modules/fail2ban_asf/manifests/config.pp
+++ modules/fail2ban_asf/manifests/config.pp
@@ -1,12 +1,12 @@
 
 class fail2ban_asf::config (
-  $filters,
-  $jails,
 ) {
 
   include fail2ban
 
+  $jails = hiera_hash('fail2ban::config::jails', {})
   create_resources(fail2ban::jail, $jails)
+  $filters = hiera_hash('fail2ban::config::filters', {})
   create_resources(fail2ban::filter, $filters)
 }
 
