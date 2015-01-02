bba57851887b6b285f1afd091fa18069168142ff
Geoffrey Corey <coreyg@apache.org>
2015-01-02 08:09:08 -0800

    Fix missing apostrophe

--- modules/rsync_mirror/manifests/init.pp
+++ modules/rsync_mirror/manifests/init.pp
@@ -48,7 +48,7 @@ class rsync_mirror (
         read_only       => 'yes',
         list            => 'yes',
         outgoing_chmod  => 'ug-s,Dugo+rx,Fugo+r,u+w,o-w,-t',
-        exclude         => ['/openoffice/4.1.0', '/openoffice/4.1.1/binaries', '/tmp/', '*.md5', '*.MD5', 'MD5SUM', 'SHA*SUM*, '*.sha1', '*.sha', '*.sha256', '*.sha512', '*.asc', '*.sig', 'KEYS', 'KEYS.txt', '.svn/', '/.rsync.td/', '/zzz/perms', '/zzz/rsync-module/apache-dist-most'],
+        exclude         => ['/openoffice/4.1.0', '/openoffice/4.1.1/binaries', '/tmp/', '*.md5', '*.MD5', 'MD5SUM', 'SHA*SUM*', '*.sha1', '*.sha', '*.sha256', '*.sha512', '*.asc', '*.sig', 'KEYS', 'KEYS.txt', '.svn/', '/.rsync.td/', '/zzz/perms', '/zzz/rsync-module/apache-dist-most'],
         hosts_deny      => $deny,
     }
 
