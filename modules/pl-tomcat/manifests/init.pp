f04b33792b3b801133cce5e9f193a18ca8289342
gmcdonald <gmcdonald@apache.org>
2015-01-16 17:39:18 +0000

    module name clash, rename, props coreyg

--- modules/pl-tomcat/manifests/init.pp
+++ modules/pl-tomcat/manifests/init.pp
@@ -1,14 +1,14 @@
-class tomcat::instance {
+class pl-tomcat::instance {
   $tcinstance = hiera_hash('tomcat::instance',{})
   create_resources(tomcat::instance, $tcinstance)
 }
 
-class tomcat::config {
+class pl-tomcat::config {
   $tcconfig = hiera_hash('tomcat::config',{})
   create_resources(tomcat::config, $tcconfig)
 }
 
-class tomcat::params {
+class pl-tomcat::params {
   $tcparams = hiera_hash('tomcat::params',{})
   create_resources(tomcat::params, $tcparams)
 }
