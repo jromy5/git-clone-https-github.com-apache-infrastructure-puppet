c1eeb5bc3160ec82b09d52fb0f32c462a4874552
Tony Stevenson <tony@pc-tony.com>
2015-01-21 16:34:43 +0000

    Deploy file and add service monitor

--- modules/ldapserver/manifests/install/ubuntu/1404.pp
+++ modules/ldapserver/manifests/install/ubuntu/1404.pp
@@ -3,4 +3,14 @@
 class ldapserver::install::ubuntu::1404 (
 
 ) { 
+
+  file { '/etc/ldap/slapd.conf': 
+    content   => template('ldapserver/slapd.conf.erb'), 
+    notify    => Service["slapd"],
+   }
+
+
+   service { 'slapd':
+     hasrestart   =>  true
+   }
 }
