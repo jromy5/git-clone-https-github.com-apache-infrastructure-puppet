bf1391ad8140562bc563ca3c4268f60ff132f5df
Tony Stevenson <tony@pc-tony.com>
2015-01-21 16:25:51 +0000

    add used template vars to the manifest to perform the hiera lookup

--- modules/ldapserver/manifests/init.pp
+++ modules/ldapserver/manifests/init.pp
@@ -2,6 +2,23 @@
 
 class ldapserver (
   $packages         = [],
+  $slapd_peers      = [],
+  $schemas          = [],
+  $loglevel         = 'stats',
+  $modulepath       = '/usr/lib/ldap',
+  $modules          = [],
+  $sizelimit        = 'unlimited',
+  $backend          = 'mdb',
+  $database         = 'mdb',
+  $suffix           = 'dc=apache,dc=org',
+  $directory        = '/var/lib/ldap',
+  $rootdn           = 'cn=root,dc=apache,dc=org',
+  $maxsize          = '1024000000',
+  $indexes          = [],
+  $cafile           = '/etc/ldap/cacerts/cacert.pem',
+  $certfile         = '/etc/ldap/cacerts/ldap-wildcard-cert.pem',
+  $keyfile          = '/etc/ldap/cacerts/ldap-wildcard-cert.key',
+
 ) {
 
   package { $packages:
@@ -9,6 +26,7 @@ class ldapserver (
   }
 
   class { "ldapserver::install::${asfosname}::${asfosrelease}":
+
   }
 
 }
