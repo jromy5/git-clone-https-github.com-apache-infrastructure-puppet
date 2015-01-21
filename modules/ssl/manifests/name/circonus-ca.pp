c734fe354cb88ee16041fdbc8f1652029f4701bc
Chris Lambertus <cml@apache.org>
2015-01-21 08:42:22 -0800

    add circonus-ca.pp for deploying circonus CA cert to collectd hosts

---
+++ modules/ssl/manifests/name/circonus-ca.pp
@@ -0,0 +1,32 @@
+#/etc/puppet/modules/manifests/ssl/name/circonus-ca.pp
+
+class ssl::name::circonus-ca (
+
+  $sslcertcontents             = '',
+  $sslcertname                 = 'circonus-ca.crt',
+  $sslchaincontent             = '',
+  $sslchainname                = '',
+  $sslkeycontents              = '',
+  $sslkeyname                  = '',
+  $sslcombinedcontents         = '',
+  $sslcombinedname             = '',
+  $sslrootdir                  = '/etc/collectd',
+  $sslrootdirgroup             = 'root',
+  $sslrootdirowner             = 'root',
+  $sslrootdirumask             = '0755',
+) {
+
+  file { 
+    "${sslrootdir}":
+      ensure   =>  directory,
+      group    =>  "${sslrootdirgroup}",
+      owner    =>  "${sslrootdirowner}",
+      mode     =>  "${sslrootdirumask}";
+    "${sslrootdir}/${sslcertname}":
+      require  =>  File["${sslrootdir}"],
+      ensure   =>  present,
+      content  =>  $sslcertcontents,
+      owner    =>  "${sslrootdirowner}",
+      group    =>  "${sslrootdirgroup}";
+  }
+}
