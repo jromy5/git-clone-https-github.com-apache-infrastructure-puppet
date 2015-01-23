7be02dceaca4fc3eaee9a00f6bda67597efc5911
Geoffrey Corey <coreyg@apache.org>
2015-01-23 12:38:10 -0800

    Add resource for firewallchain

--- modules/firewall_asf/manifests/init.pp
+++ modules/firewall_asf/manifests/init.pp
@@ -3,4 +3,7 @@ class firewall_asf (
 
   $firewall = hiera_hash('firewall',{})
   create_resources(firewall, $firewall)
+
+  $firewallchain = hiera_hash('firewallchain', {})
+  create_resources(firewallchain, $firewallchain)
 }
