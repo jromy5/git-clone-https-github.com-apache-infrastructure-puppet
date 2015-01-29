03805c495c5c65711468fb1d7ef596f742a2e06d
Tony Stevenson <tony@pc-tony.com>
2015-01-29 17:41:33 +0000

    Add support for the mail_relay components specifically

---
+++ modules/mail_relay/manifests/init.pp
@@ -0,0 +1,16 @@
+#/etc/puppet/modules/mail_relay/manifests/init.pp
+
+class mail_relay (
+
+  $mail_ldap_servers     = '',  
+
+) {
+
+  file {
+    '/etc/postfix/ldap-mail-forward-lookup.cf':
+      source  => content('mail_rely/ldap-mail-forward-lookup.cf.erb'),
+      mode    => '0644',
+      notify  => 'postfix',
+      require => Class['postfix::server'];
+  }
+}
