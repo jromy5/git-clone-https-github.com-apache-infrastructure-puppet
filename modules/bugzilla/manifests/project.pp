09118c17680bd7dce55fd1c392d311ac9a958d38
Geoffrey Corey <coreyg@apache.org>
2015-02-09 15:16:12 -0800

    fix typo

--- modules/bugzilla/manifests/project.pp
+++ modules/bugzilla/manifests/project.pp
@@ -111,9 +111,9 @@ define bugzilla::project (
   }
 
   cron { "daily_update_of_${name}_graphs":
-    command => "${docroot}/collectstats.pl 2&1 | grep -v Use\ of\ uninitialized.*line\ 31",
-    minute  => 5,
-    hour    => 0,
+    command    => "${docroot}/collectstats.pl 2>&1 | grep -v Use\ of\ uninitialized.*line\ 31",
+    minute     => 5,
+    hour       => 0,
    environment => 'PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SHELL=/bin/sh',
     require => Class['rootbin_asf'],
