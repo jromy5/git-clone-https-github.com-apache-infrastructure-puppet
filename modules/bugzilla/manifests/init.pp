ea84b46eff9955e70e677cde9db22b9e00805358
Geoffrey Corey <coreyg@apache.org>
2015-02-09 14:41:09 -0800

    Add bugcron

--- modules/bugzilla/manifests/init.pp
+++ modules/bugzilla/manifests/init.pp
@@ -3,12 +3,24 @@ class bugzilla (
 ) {
 
   require apache
+  require rootbin_asf
 
   file { ["/etc/bugzilla", "/etc/bugzilla/.puppet"]:
     ensure => directory,
     mode   => 0755,
     owner  => "root",
     group  => "root",
+  }
+
+  cron { 'bugcron':
+    command => '/root/bin/bugcron.sh',
+    user    => 'root',
+    minute  => 15,
+    hour    => 7,
+    weekday => 0,
+   environment => 'PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SHELL=/bin/sh',
+    require => Class['rootbin_asf'],
   }
 
 }
