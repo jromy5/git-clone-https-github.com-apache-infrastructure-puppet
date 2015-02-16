0f17e5d85d63f8e06d67d77a6814f3256984047a
Andrew Bayer <andrew.bayer@gmail.com>
2015-02-16 13:08:39 -0800

    Add maven_central_mirror_asf class, use it

---
+++ modules/maven_central_mirror_asf/manifests/install.pp
@@ -0,0 +1,31 @@
+class maven_central_mirror_asf::install {
+
+  $maven_central_mirror_pkgs = [
+                                'awscli'
+  ]
+
+  if $::lsbdistcodename == 'trusty' {
+
+    package { $maven_central_mirror_pkgs:
+      ensure  => 'installed',
+    }
+  }
+
+  s3fs::mount { 'asf-maven-central-mirror':
+    bucket      => 'asf-maven-central-mirror',
+    mount_point => '/mnt/asf-maven-central-mirror',
+    ensure      => defined,
+    before      => Exec['mount s3fs'],
+  }
+
+  exec { "mount s3fs":
+    command => "/bin/mount /mnt/asf-maven-central-mirror",
+    unless  => "/bin/grep -qs asf-maven-central-mirror /etc/mtab",
+    require => S3fs::Mount['asf-maven-central-mirror']
+  } 
+
+  exec { "unmount s3fs":
+    command => "/bin/umount /mnt/asf-maven-central-mirror",
+  }
+
+}
