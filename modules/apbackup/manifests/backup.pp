ab098555bf7df36b5943f2d64061caefa4c8aa17
Geoffrey Corey <coreyg@apache.org>
2015-02-18 10:39:17 -0800

    Add support for apbackup ssh key

---
+++ modules/apbackup/manifests/backup.pp
@@ -0,0 +1,28 @@
+
+class apbackup::backup (
+  $ssh_key = '',
+) inherits apbackup {
+
+  cron { 'daily-www-push':
+    minute  => 10,
+    hour    => 1,
+    command => "/home/apbackup/bin/daily-www-push.sh 2&1 | perl -lne 'print if $. < 10'",
+    require => User["${apbackup::username}"],
+  }
+
+  file { "/home/${apbackup::username}/.ssh":
+    ensure  => directory,
+    owner   => "${apbackup::username}",
+    group   => "${apbackup::groupname}",
+    mode    => '0700',
+    require => User["${apbackup::username}"],
+  }
+
+  file { "/home/${apbackup::username}/.ssh/id_rsa":
+    ensure  => file,
+    owner   => "${apbackup::username}",
+    group   => "${apbackup::groupname}",
+    mode    => '0600',
+    content => $ssh_key,
+  }
+}
