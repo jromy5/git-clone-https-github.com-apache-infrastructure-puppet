6ddf771e2613be93f4eee289927bf1c34be6f182
Daniel Gruno <humbedooh@apache.org>
2015-02-19 10:02:32 +0100

    initial commit of dist archive module

---
+++ modules/dist_archive_asf/manifests/init.pp
@@ -0,0 +1,85 @@
+#/etc/puppet/modules/dist_archive_asf/manifests/init.pp
+
+class archive (
+  $uid           = 1025,
+  $gid           = 1025,
+  $group_present = 'present',
+  $groupname     = 'archive',
+  $groups        = [],
+  $shell         = '/bin/bash',
+  $user_present  = 'present',
+  $username      = 'archive',
+  $archiveroot   = '/var/www/archive.apache.org',
+  $rsync_server  = 'rsync.apache.org',
+) {
+
+  
+   user { "${username}":
+        name       => "${username}",
+        ensure     => "${user_present}",
+        home       => "/home/${username}",
+        shell      => "${shell}",
+        uid        => "${uid}",
+        gid        => "${groupname}",
+        groups     => $groups,
+        managehome => true,
+        require    => Group["${groupname}"],
+    }
+
+    group { "${groupname}":
+        name   => "${groupname}",
+        ensure => "${group_present}",
+        gid    => "${gid}",
+    }
+
+    file { 'archive profile': 
+        path    => "/home/${username}/.profile",
+        ensure  => 'present',
+        mode    => '0644',
+        owner   => "${username}",
+        group   => "${groupname}",
+        source  => 'puppet:///modules/dist_archive_asf/home/profile',
+        require => User["${username}"],
+    }
+    
+     file { 'archive root':
+        ensure => directory,
+        path => "${archiveroot}",
+        mode   => 0755,
+        owner  => "${username}",
+        group  => "${groupname}",
+    }
+    
+    file { 'archive dist dir':
+        ensure => directory,
+        path => "${archiveroot}/dist",
+        mode   => 0755,
+        owner  => "${username}",
+        group  => "${groupname}",
+    }
+    
+    file { 'archive front page': 
+        path    => "${archiveroot}/index.html",
+        ensure  => 'present',
+        mode    => '0644',
+        owner   => "${username}",
+        group   => "${groupname}",
+        source  => 'puppet:///modules/dist_archive_asf/index.html',
+        require => User["${username}"],
+    }
+       
+    
+    package { 'rsync':
+      ensure => installed
+    }
+    
+    class { 'rsync': package_ensure => 'latest' }
+    
+    rsync::get { "${archiveroot}/dist":
+      source  => "rsync://${rsync_server}/apache-dist-for-archive/",
+      require => File["${archiveroot}/dist"],
+      chown => "${username}:${groupname}",
+    }
+    
+    
+}
