b57d110fa60fea88b917a69cc0861811886ef3e8
gmcdonald <gmcdonald@apache.org>
2015-03-12 18:27:09 +0000

    custom fragment for now, revisit later

--- modules/cwiki_asf/manifests/init.pp
+++ modules/cwiki_asf/manifests/init.pp
@@ -160,7 +160,8 @@
         { 'path' => '/confluence', 'url' => 'http://127.0.0.1:8888/confluence',
           'reverse_urls' => ['http://127.0.0.1:8888/confluence'] },
       ],
-          no_proxy_uris => ['/intermediates'],
+      #    no_proxy_uris => ['/intermediates'],
+      custom_fragment => 'ProxyPass /intermediates !'
   }
 
   service { "${service_name}":
