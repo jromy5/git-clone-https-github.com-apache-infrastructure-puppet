7049010fd82ef9bc957708e98b5e2dd2eacfbc59
Geoffrey Corey <coreyg@apache.org>
2015-03-18 10:36:07 -0700

    add support for rax vms for now

--- modules/customfact/lib/facter/customfact.rb
+++ modules/customfact/lib/facter/customfact.rb
@@ -29,34 +29,39 @@ end
 
 Facter.add("asfcolo") do
   setcode do
-    ipadd = Facter.value('ipaddress_primary')
-    case ipadd
-    when /^140.211.11.([0-9]+)$/
-      "osuosl"
-    when /^192.87.106.([0-9]+)$/
-      "sara"
-    when /^160.45.251.([0-9]+)$/
-      "fub"
-    when /^9.9.9.([0-9]+)$/
-      "rackspace"
-    when /^67.195.81..([0-9]+)$/
-      "yahoo"
-    when /^172\.31\.3[2-9]|4[0-7]\.\d+$/
-      "amz-vpc-virginia-1b"
-    when /^10.0.([0-9]+).([0-9]+)$/
-      "amz-vpc-virginia-1d"
-    when /^10.3.([0-9]+).([0-9]+)$/
-      "amz-vpc-us-west"
-    when /^10.2.([0-9]+).([0-9]+)$/
-      "amz-vpc-eu-west"
-    when /^10.30.([0-9]+).([0-9]+)$/
-      "amz-vpc-eu-central"
-    when /^162.209.6.([0-9]+)$/
+    hostname = Facter.value('hostname')
+    if hostname.include? "us-mid"
       "rax-vpc-us-mid"
-    when /^10.41.([0-9]+).([0-9]+)$/
-      "phoenixnap"
     else
-      "yahoo"
+      ipadd = Facter.value('ipaddress_primary')
+      case ipadd
+      when /^140.211.11.([0-9]+)$/
+        "osuosl"
+      when /^192.87.106.([0-9]+)$/
+        "sara"
+      when /^160.45.251.([0-9]+)$/
+        "fub"
+      when /^9.9.9.([0-9]+)$/
+        "rackspace"
+      when /^67.195.81..([0-9]+)$/
+        "yahoo"
+      when /^172\.31\.3[2-9]|4[0-7]\.\d+$/
+        "amz-vpc-virginia-1b"
+      when /^10.0.([0-9]+).([0-9]+)$/
+        "amz-vpc-virginia-1d"
+      when /^10.3.([0-9]+).([0-9]+)$/
+        "amz-vpc-us-west"
+      when /^10.2.([0-9]+).([0-9]+)$/
+        "amz-vpc-eu-west"
+      when /^10.30.([0-9]+).([0-9]+)$/
+        "amz-vpc-eu-central"
+      when /^162.209.6.([0-9]+)$/
+        "rax-vpc-us-mid"
+      when /^10.41.([0-9]+).([0-9]+)$/
+        "phoenixnap"
+      else
+        "yahoo"
+      end
     end
   end
 end
