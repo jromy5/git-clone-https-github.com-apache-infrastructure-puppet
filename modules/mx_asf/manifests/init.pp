57d645c6f8a0b1c0d5b23121235d7ab0c4d596ec
Tony Stevenson <tony@pc-tony.com>
2015-04-08 00:04:06 +0100

    USe double quotes to try and force the newline

--- modules/mx_asf/manifests/init.pp
+++ modules/mx_asf/manifests/init.pp
@@ -5,7 +5,7 @@ class mx_asf  (
 
      postfix::file { 'recipient_canonical_maps':
        ensure  => present,
-       content => "## This file is managed by puppet - all local changes will be lost\n\n/^(.*)@(.*).incubator.apache.org$/ ${1}@${2}.apache.org",
+       content => "## This file is managed by puppet - all local changes will be lost\n\n/^(.*)@(.*).incubator.apache.org$/ $\{1\}@$\{2\}.apache.org",
      }
 
 }
