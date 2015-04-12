02e64232677e5e16f3c96f70374dbca10592f907
Chris Lambertus <cml@apache.org>
2015-04-12 10:59:46 -0700

    fix class syntax, add class to lucene node

--- modules/lucene1-pvm_asf/manifests/init.pp
+++ modules/lucene1-pvm_asf/manifests/init.pp
@@ -1,8 +1,14 @@
-user { 'jenkins':
-  name     => 'jenkins',
-  ensure   => present,
-  comment  => 'lucene project VM jenkins slave',
-  home     => '/home/jenkins',
-  uid      => '3082',
+class lucene-pvm_asf {
+
+  # manifest for lucene project vm
+
+  user { 'jenkins':
+    name     => 'jenkins',
+    ensure   => present,
+    comment  => 'lucene project VM jenkins slave',
+    home     => '/home/jenkins',
+    uid      => '3082',
+  }
 }
+
 
