0d376afae71df63416b9f7dfaad3752c1f258d39
Chris Lambertus <cml@apache.org>
2015-04-12 14:05:28 -0700

    create local jenkins homedir

--- modules/lucene1-pvm_asf/manifests/init.pp
+++ modules/lucene1-pvm_asf/manifests/init.pp
@@ -3,11 +3,12 @@ class lucene1-pvm_asf {
   # manifest for lucene project vm
 
   user { 'jenkins':
-    name     => 'jenkins',
-    ensure   => present,
-    comment  => 'lucene project VM jenkins slave',
-    home     => '/home/jenkins',
-    uid      => '800',
+    name       => 'jenkins',
+    ensure     => present,
+    comment    => 'lucene project VM jenkins slave',
+    home       => '/home/jenkins',
+    managehome => true,
+    uid        => '800',
   }
 }
 
