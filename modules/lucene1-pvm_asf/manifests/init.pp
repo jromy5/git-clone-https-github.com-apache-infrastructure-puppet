8c6b8b526320ffd60331261ed8cd9eade41294d2
Chris Lambertus <cml@apache.org>
2015-04-23 08:30:51 -0700

    pin SVN 1.6 from precise to 14.04 and force update force is required to avoid manual intervention since svn is already installed

--- modules/lucene1-pvm_asf/manifests/init.pp
+++ modules/lucene1-pvm_asf/manifests/init.pp
@@ -10,6 +10,39 @@ class lucene1-pvm_asf {
     managehome => true,
     uid        => '800',
   }
+
+  apt::source { 'precise':
+    location => 'http://us.archive.ubuntu.com/ubuntu/',
+    release  => 'precise',
+    repos    => 'main',
+  }
+
+  apt::source { 'precise-updates':
+    location => 'http://us.archive.ubuntu.com/ubuntu/',
+    release  => 'precise-updates',
+    repos    => 'main',
+  }
+
+  apt::pin { 'precise-subversion': 
+    priority => 1800,
+    ensure   => present,
+    packages => 'subversion',
+    codename => 'precise',
+  }
+  
+  apt::pin { 'precise-libsvn1': 
+    priority => 1800,
+    ensure   => present,
+    packages => 'libsvn1',
+    codename => 'precise',
+  }
+
+  exec { 'upgrade-svn':
+    command => '/usr/bin/apt-get -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade subversion',
+    require => Apt::Pin['precise-subversion'],
+  }
+
+
 }
 
 
