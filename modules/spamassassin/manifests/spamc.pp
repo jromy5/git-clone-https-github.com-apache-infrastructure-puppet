caa16b131998d6a57deefe68d000f980d2382342
pctony <tony@pc-tony.com>
2015-04-27 09:13:45 +0100

    Add support for pulling the pasword in from eyaml

--- modules/spamassassin/manifests/spamc.pp
+++ modules/spamassassin/manifests/spamc.pp
@@ -1,16 +1,20 @@
 class spamassassin::spamc (
 
-  $spamd_peers       = '',
-  $haproxy_maxconns  = '',
-  $haproxy_port      = '',
-  $haproxy_mode      = 'tcp',
+  $spamd_peers           = '',
+  $haproxy_maxconns      = '',
+  $haproxy_port          = '',
+  $haproxy_mode          = 'tcp',
+  $haproxy_statuser      = '',
+  $haproxy_statspassword = '',
 
 ) { 
 
   class { "spamassassin::spamc::install::${asfosname}::${asfosrelease}":
-    spamd_peers       => $spamd_peers,
-    haproxy_maxconns  => $haproxy_maxconns,
-    haproxy_port      => $haproxy_port,
-    haproxy_mode      => $haproxy_mode,
+    spamd_peers           => $spamd_peers,
+    haproxy_maxconns      => $haproxy_maxconns,
+    haproxy_port          => $haproxy_port,
+    haproxy_mode          => $haproxy_mode,
+    haproxy_statsuser     => $haproxy_statsuser,
+    haproxy_statspassword => $haproxy_statspassword,
   }
 }
