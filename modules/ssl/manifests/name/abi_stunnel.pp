1060250a42014db05afccf56ab44c4a7c3c6fd0c
Geoffrey Corey <coreyg@apache.org>
2015-04-27 18:04:50 -0700

    forogt to add this back in

---
+++ modules/ssl/manifests/name/abi_stunnel.pp
@@ -0,0 +1,39 @@
+#/etc/puppet/modules/manifests/ssl/name/abi_stunnel.pp
+
+class ssl::name::abi_stunnel (
+
+  $sslcertcontents     = '',
+  $sslcertname         = 'abi.apache.org.crt',
+  $sslchaincontent     = '',
+  $sslchainname        = '',
+  $sslkeycontents      = '',
+  $sslkeyname          = '',
+  $sslcombinedcontents = '',
+  $sslcombinedname     = '',
+  $sslrootdir          = '/etc/ssl',
+  $sslrootdirgroup     = 'root',
+  $sslrootdirowner     = 'root',
+  $sslrootdirumask     = '0755',
+) {
+
+  file {
+    $sslrootdir:
+      ensure => directory,
+      group  => $sslrootdirgroup,
+      owner  => $sslrootdirowner,
+      mode   => $sslrootdirumask;
+    "${sslrootdir}/certs":
+      ensure   =>  directory,
+      group    =>  $sslrootdirgroup,
+      owner    =>  $sslrootdirowner,
+      mode     =>  '0755';
+    "${sslrootdir}/certs/${sslcertname}":
+      nsure   => present,
+      require => File[$sslrootdir],
+      content => $sslcertcontents,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+  }
+
+}
+
