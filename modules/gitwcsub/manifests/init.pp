b71ad919d1f934d2078108afc47086ed42cf26f9
Geoffrey Corey <coreyg@apache.org>
2015-04-29 13:31:49 -0700

    add logrotate rule for gitwcsub

--- modules/gitwcsub/manifests/init.pp
+++ modules/gitwcsub/manifests/init.pp
@@ -75,4 +75,17 @@ class gitwcsub (
         hasstatus => false,
     }
 
+    logrotate::rule { 'gitwcsub':
+      path         => '/var/log/gitwcsub/gitwcsub-currentl.log',
+      rotate       => 7,
+      rotate_every => 'day',
+      compress     => true,
+      create       => true,
+      create_owner => $username,
+      create_group => $groupname,
+      create_mode  => '0644',
+      dateext      => true,
+      missingok    => true,
+    }
+
 }
