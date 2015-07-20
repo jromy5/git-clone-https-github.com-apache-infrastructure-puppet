827f3882381e8c6bc10b4d34293eacb600637608
pctony <tony@pc-tony.com>
2015-07-20 21:48:40 +0100

    Add Ilias Paris 'colo'

--- modules/customfact/lib/facter/customfact.rb
+++ modules/customfact/lib/facter/customfact.rb
@@ -62,6 +62,8 @@ Facter.add("asfcolo") do
         "phoenixnap-public"
       when /^10.40.([0-9]+).([0-9]+)$/
         "phoenixnap-private"
+      when /^195.154.223.([0-9]+)$/
+        "iliad-paris"
       else
         "yahoo"
       end
