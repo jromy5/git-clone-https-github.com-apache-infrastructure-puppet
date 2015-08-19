ada27be2f065266512230ce19d0746630fd3d752
Geoffrey Corey <coreyg@apache.org>
2015-08-19 16:21:45 -0700

    Squashed commit of the following:
    commit 7eb3364b7551684815717e16424d7395372b2fe2
    Author: humbedooh <humbedooh@apache.org>
    Date:   Thu Aug 20 01:18:21 2015 +0200
    
        style
    
    commit c25d28f7f6fadb4ee7e38ed4f39c8cbbf6c7386d
    Author: humbedooh <humbedooh@apache.org>
    Date:   Thu Aug 20 01:16:05 2015 +0200
    
        fix all geoff's thingies
    
    commit 4c83d0d4897d965ba0e5ac803fd4f344393b38ec
    Author: humbedooh <humbedooh@apache.org>
    Date:   Thu Aug 20 01:09:45 2015 +0200
    
        fix lua lib paths again
    
    commit 3a21c60293deee9c1abe26dbc25b8488bcf88ef4
    Author: humbedooh <humbedooh@apache.org>
    Date:   Thu Aug 20 01:09:06 2015 +0200
    
        fix lua lib paths
    
    commit 713e7b9b81dc026a3290ada08c14efd972e8161f
    Author: humbedooh <humbedooh@apache.org>
    Date:   Thu Aug 20 01:06:31 2015 +0200
    
        add kibana 3 module

---
+++ modules/kibana_asf/manifests/init.pp
@@ -0,0 +1,48 @@
+#/etc/puppet/modules/kibana_asf/manifests/init.pp
+
+class kibana_asf (
+    $packages = ['lua5.2']
+  ) {
+
+  package { $packages:
+    ensure => present,
+  }
+
+  file {
+    "/usr/local/etc/logproxy":
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root';
+    "/usr/local/etc/logproxy/frombrowser.lua":
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/kibana_asf/frombrowser.lua";
+    "/usr/local/etc/logproxy/tobrowser.lua":
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/kibana_asf/tobrowser.lua";
+    "/usr/local/etc/logproxy/JSON.lua":
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/kibana_asf/JSON.lua";
+  } ->
+
+
+  exec { 'download kibana 3':
+    command => '/usr/bin/curl -o /tmp/kibana.tgz https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz', # lint:ignore:80chars
+    unless  => 'ls /tmp/ | grep -qs kibana.tgz',
+  } ->
+
+  exec { 'untar kibana':
+    command => '/bin/mkdir -p /usr/local/etc/logproxy/kibana && /bin/tar -C /usr/local/etc/logproxy/kibana -xzf /tmp/kibana.tgz --strip 1', # lint:ignore:80chars
+  } ->
+
+  exec { 'sed kibana config':
+    command => '/bin/sed "s/:9200\"/\/_query\", withCredentials: true}/" < /usr/local/etc/logproxy/kibana/config.js | /bin/sed "s/http/https/g" > /usr/local/etc/logproxy/kibana/kibana/config.js', # lint:ignore:80chars
+  }
+
+}
