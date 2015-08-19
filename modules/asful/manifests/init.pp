2aea90e8ef30095369ea019fc43ad7c8f83e8551
Daniel Gruno <humbedooh@apache.org>
2015-08-19 15:35:45 +0200

    Start adding elastic stuff

---
+++ modules/asful/manifests/init.pp
@@ -0,0 +1,25 @@
+#/etc/puppet/modules/asful/manifests/init.pp
+
+class asful (
+  $nodename       = 'ul1-eu-central',
+  $nodeip         = '10.91.64.150'
+
+){
+
+  file {
+    ["/etc/elasticsearch", "/etc/elasticsearch/config"]:
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',    
+    }
+    
+  file {
+    "/etc/elasticsearch/config/elasticsearch.yml":
+      ensure => file,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+      content => template('asful/yaml.erb'),
+    }
+}
