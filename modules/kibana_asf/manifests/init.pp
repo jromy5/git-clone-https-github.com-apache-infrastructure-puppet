c25d28f7f6fadb4ee7e38ed4f39c8cbbf6c7386d
humbedooh <humbedooh@apache.org>
2015-08-20 01:16:05 +0200

    fix all geoff's thingies

--- modules/kibana_asf/manifests/init.pp
+++ modules/kibana_asf/manifests/init.pp
@@ -1,9 +1,9 @@
 #/etc/puppet/modules/kibana_asf/manifests/init.pp
 
-class kibana_asf {
+class kibana_asf ($packages = ['lua5.2']) {
 
-  package { [ 'lua5.2', 'lua-filesystem', 'lua-socket'] :
-    ensure => installed
+  package { $packages:
+    ensure => present,
   }
 
   file {
@@ -33,18 +33,14 @@ class kibana_asf {
   exec { 'download kibana 3':
     command => '/usr/bin/curl -o /tmp/kibana.tgz https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz', # lint:ignore:80chars
     unless  => 'ls /tmp/ | grep -qs kibana.tgz',
-    before  => Exec['untar kibana'],
   } ->
 
   exec { 'untar kibana':
     command => '/bin/mkdir -p /usr/local/etc/logproxy/kibana && /bin/tar -C /usr/local/etc/logproxy/kibana -xzf /tmp/kibana.tgz --strip 1', # lint:ignore:80chars
-    require => Exec['download kibana 3'],
-    before  => Exec['sed kibana config'],
   } ->
 
   exec { 'sed kibana config':
     command => '/bin/sed "s/:9200\"/\/_query\", withCredentials: true}/" < /usr/local/etc/logproxy/kibana/config.js | /bin/sed "s/http/https/g" > /usr/local/etc/logproxy/kibana/kibana/config.js', # lint:ignore:80chars
-    require => Exec['untar kibana'],
   }
 
 }
