40ecd56af69b447f93c2d130ef3bbc0929785dd7
humbedooh <humbedooh@apache.org>
2015-08-22 13:59:36 +0200

    Rearrange and shorten log paths. Add license header.

--- modules/loggy/files/loggy.py
+++ modules/loggy/files/loggy.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Loggy (Jr) - A log file gobbler
+#
+# Example:
+#  loggy.py --daemonize
+#
+
 
 from inotify import watcher
 import inotify
@@ -18,6 +41,8 @@ import subprocess, collections, argparse, grp, pwd, shutil
 
 # ElasticSearch
 from elasticsearch import Elasticsearch, helpers
+
+paths = ['/var/log/', '/x1/log']
 
 es = None
 hostname = socket.gethostname()
@@ -203,7 +228,7 @@ class Daemonize:
 w = watcher.AutoWatcher()
 
 
-paths = ['/var/log/apache2/', '/var/log/', '/var/log/tomcat/', '/x1/log']
+
 filehandles = {}
 json_pending = {}
 last_push = {}
