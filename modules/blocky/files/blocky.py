4349d03edf10950133ae62b15c58835180cbf566
humbedooh <humbedooh@apache.org>
2015-08-24 14:49:23 +0200

    Add Blocky module to puppet

---
+++ modules/blocky/files/blocky.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Blocky - ES aggregated ban thingy
+#
+# Example:
+#  blocky.py --daemonize
+#  requires blocky.cfg
+
+import os
+import sys
+import time, datetime
+import json
+import re
+from threading import Thread
+import random, atexit, signal, inspect
+from threading import Lock
+import subprocess, collections, argparse, grp, pwd, shutil
+import ConfigParser
+import smtplib
+import socket
+import urllib
+
+config = ConfigParser.ConfigParser()
+
+es = None
+hostname = socket.gethostname()
+if hostname.find(".apache.org") == -1:
+    hostname = hostname + ".apache.org"
+
+
+class Daemonize:
+    """A generic daemon class.
+
+    Usage: subclass the daemon class and override the run() method."""
+
+    def __init__(self, pidfile): self.pidfile = pidfile
+
+    def daemonize(self):
+	"""Deamonize class. UNIX double fork mechanism."""
+
+	try:
+	    pid = os.fork()
+	    if pid > 0:
+		# exit first parent
+		sys.exit(0)
+	except OSError as err:
+	    sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+	    sys.exit(1)
+
+	# decouple from parent environment
+	os.chdir('/')
+	os.setsid()
+	os.umask(0)
+
+	# do second fork
+	try:
+	    pid = os.fork()
+	    if pid > 0:
+
+		# exit from second parent
+		sys.exit(0)
+	except OSError as err:
+	    sys.stderr.write('fork #2 failed: {0}\n'.format(err))
+	    sys.exit(1)
+
+	# redirect standard file descriptors
+	sys.stdout.flush()
+	sys.stderr.flush()
+	si = open(os.devnull, 'r')
+	so = open(os.devnull, 'a+')
+	se = open(os.devnull, 'a+')
+
+	os.dup2(si.fileno(), sys.stdin.fileno())
+	os.dup2(so.fileno(), sys.stdout.fileno())
+	os.dup2(se.fileno(), sys.stderr.fileno())
+
+	# write pidfile
+	atexit.register(self.delpid)
+
+	pid = str(os.getpid())
+	with open(self.pidfile,'w+') as f:
+	    f.write(pid + '\n')
+
+    def delpid(self):
+	os.remove(self.pidfile)
+
+    def start(self, args):
+	"""Start the daemon."""
+
+	# Check for a pidfile to see if the daemon already runs
+	try:
+	    with open(self.pidfile,'r') as pf:
+
+		pid = int(pf.read().strip())
+	except IOError:
+	    pid = None
+
+	if pid:
+	    message = "pidfile {0} already exist. " + \
+			    "Daemon already running?\n"
+	    sys.stderr.write(message.format(self.pidfile))
+	    sys.exit(1)
+
+	# Start the daemon
+	self.daemonize()
+	self.run(args)
+
+    def stop(self):
+	"""Stop the daemon."""
+
+	# Get the pid from the pidfile
+	try:
+	    with open(self.pidfile,'r') as pf:
+		pid = int(pf.read().strip())
+	except IOError:
+	    pid = None
+
+	if not pid:
+	    message = "pidfile {0} does not exist. " + \
+			    "Daemon not running?\n"
+	    sys.stderr.write(message.format(self.pidfile))
+	    return # not an error in a restart
+
+	# Try killing the daemon process
+	try:
+	    while 1:
+		os.kill(pid, signal.SIGTERM)
+		time.sleep(0.1)
+	except OSError as err:
+	    e = str(err.args)
+	    if e.find("No such process") > 0:
+		if os.path.exists(self.pidfile):
+		    os.remove(self.pidfile)
+	    else:
+		print (str(err.args))
+		sys.exit(1)
+
+    def restart(self):
+	"""Restart the daemon."""
+	self.stop()
+	self.start()
+
+    def run(self):
+	"""You should override this method when you subclass Daemon.
+
+	It will be called after the process has been daemonized by
+	start() or restart()."""
+
+
+
+class Blocky(Thread):
+    def run(self):
+	baddies = {}
+	while True:
+	    try:
+		js = json.loads(urllib.urlopen(config.get('aggregator','uri')).read())
+		for baddie in js:
+	    # Got a new one?? :)
+		    i = baddie['ip']
+		    ta = baddie['target']
+		    if not i in baddies and (ta == hostname or ta == '*'):
+			r = baddie['reason']
+			t = time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime())
+			try:
+			    print("Found a culprit for us, banning " + i)
+			    subprocess.check_call([
+				"iptables",
+				"-A", "INPUT",
+				"-s", i,
+				"-j", "DROP",
+				"-m", "comment",
+				"--comment",
+				"Banned by Blocky"
+				])
+			    message = """From: Blocky <blocky@%s>
+    To: Apache Infrastructure Root <root@apache.org>
+    Reply-To: root@apache.org
+    Subject: [Blocky] Banned %s on %s.
+    
+    Hi, this is %s.
+    I have just blocked %s from accessing this machine due to violations on
+    this or another server: "%s"
+    
+    This was done via the Blocky aggregator.
+    
+    To remove this ban, run the following command:
+    
+    sudo iptables -D INPUT -s %s -j DROP -m comment --comment "Banned by Blocky"
+    
+    With regards,
+    Blocky.
+    """ % (hostname, i, hostname, hostname, i, r, i)
+			    smtpObj = smtplib.SMTP('localhost')
+			    smtpObj.sendmail("blocky@" + hostname, ['root@apache.org'], message)
+			    
+			except Exception as err:
+			    print(err)
+			baddies[i] = True
+		    elif i in baddies and (ta == hostname or ta == '*') and 'unban' in baddie and baddie['unban'] == True:
+			r = baddie['reason']
+			t = time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime())
+			try:
+			    print("Leniency time! Unbanning " + i)
+			    subprocess.check_call([
+				"iptables",
+				"-D", "INPUT",
+				"-s", i,
+				"-j", "DROP",
+				"-m", "comment",
+				"--comment", "Banned by Blocky"
+				])
+			    message = """From: Blocky <blocky@%s>
+    To: Apache Infrastructure Root <root@apache.org>
+    Reply-To: root@apache.org
+    Subject: [Blocky] Unbanned %s on %s.
+    
+    Hi, this is %s.
+    I have just unbanned %s on this machine due to leniency
+    from the Blocky master server.
+    
+    With regards,
+    Blocky.
+    """ % (hostname, i, hostname, hostname, i)
+			    smtpObj = smtplib.SMTP('localhost')
+			    smtpObj.sendmail("blocky@" + hostname, ['root@apache.org'], message)
+			    
+			except Exception as err:
+			    print(err)
+			del baddies[i]
+		time.sleep(180)
+	    except:
+		pass
+
+
+
+parser = argparse.ArgumentParser(description='Command line options.')
+parser.add_argument('--user', dest='user', type=str, nargs=1,
+		   help='Optional user to run Blocky as')
+parser.add_argument('--group', dest='group', type=str, nargs=1,
+		   help='Optional group to run Blocky as')
+parser.add_argument('--pidfile', dest='pidfile', type=str, nargs=1,
+		   help='Optional pid file location')
+parser.add_argument('--daemonize', dest='daemon', action='store_true',
+		   help='Run as a daemon')
+parser.add_argument('--stop', dest='kill', action='store_true',
+		   help='Kill the currently running Blocky process')
+args = parser.parse_args()
+
+pidfile = "/var/run/blocky.pid"
+if args.pidfile and len(args.pidfile) > 2:
+    pidfile = args.pidfile
+
+def main():
+
+    if args.group and len(args.group) > 0:
+	gid = grp.getgrnam(args.group[0])
+	os.setgid(gid[2])
+
+    if args.user and len(args.user) > 0:
+	print("Switching to user %s" % args.user[0])
+	uid = pwd.getpwnam(args.user[0])[2]
+	os.setuid(uid)
+
+    global pending
+    blocky = Blocky()
+    blocky.start()
+
+
+## Daemon class
+class MyDaemon(Daemonize):
+    def run(self, args):
+	main()
+
+# Get started!
+if args.kill:
+    print("Stopping Blocky")
+    daemon = MyDaemon(pidfile)
+    daemon.stop()
+else:
+    config.read("blocky.cfg")
+
+    if args.daemon:
+	print("Daemonizing...")
+	daemon = MyDaemon(pidfile)
+	daemon.start(args)
+    else:
+	main()
---
+++ modules/blocky/manifests/init.pp
@@ -0,0 +1,44 @@
+#/etc/puppet/modules/blocky/manifests/init.pp
+
+class blocky (
+  $service_name   = 'blocky',
+  $shell          = '/bin/bash',
+  $service_ensure = 'running',
+  $username       = 'root',
+  $group          = 'root',
+){
+  require python
+
+  file {
+    '/usr/local/etc/blocky':
+      ensure => directory,
+      mode   => '0755',
+      owner  => $username,
+      group  => $group;
+    '/etc/init.d/blocky':
+      mode   => '0755',
+      owner  => $username,
+      group  => $group,
+      source => "puppet:///modules/blocky/blocky.${::asfosname}";
+    '/usr/local/etc/blocky/blocky.py':
+      mode   => '0755',
+      owner  => $username,
+      group  => $group,
+      source => 'puppet:///modules/blocky/blocky.py';
+    '/usr/local/etc/blocky/blocky.cfg':
+      mode   => '0755',
+      owner  => $username,
+      group  => $group,
+      source => 'puppet:///modules/blocky/blocky.cfg';
+    } ->
+
+    service { $service_name:
+        ensure    => $service_ensure,
+        enable    => true,
+        hasstatus => true,
+        subscribe => [
+          File['/usr/local/etc/blocky/blocky.py'],
+          File['/usr/local/etc/blocky/blocky.cfg']
+        ]
+    }
+}
