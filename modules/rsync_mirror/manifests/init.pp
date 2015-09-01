aba6eeaa8b062eb76991e177cedbc620e6790d9e
Geoffrey Corey <coreyg@apache.org>
2015-09-01 09:36:17 -0700

    fix shell for rsync_hang script

--- modules/rsync_mirror/manifests/init.pp
+++ modules/rsync_mirror/manifests/init.pp
@@ -26,7 +26,7 @@ class rsync_mirror (
   cron { 'kill stale rsync':
     ensure      => present,
     command     => 'to_kill=$(/usr/local/bin/rsync_hang.pl | tail -1) && if [[ ! -z $to_kill ]] ; then kill -15 $to_kill ; fi', # lint:ignore:80chars
-    environment => 'PATH=/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    environment => 'PATH=/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin SHELL=/bin/bash',
     hour        => '0',
     minute      => '15',
     user        => 'root',
