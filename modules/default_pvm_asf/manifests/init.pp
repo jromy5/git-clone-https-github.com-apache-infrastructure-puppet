d474941488278ac1fcb5f92fe45c1b72cb751269
gmcdonald <gmcdonald@apache.org>
2015-09-15 10:51:18 +0100

    Add tcsh

--- modules/default_pvm_asf/manifests/init.pp
+++ modules/default_pvm_asf/manifests/init.pp
@@ -2,7 +2,7 @@
 
 class default_pvm_asf (
 
-  $required_packages = ['joe' , 'ant' , 'unzip' , 'tomcat7'],
+  $required_packages = ['joe' , 'ant' , 'unzip' , 'tomcat7' , 'tcsh'],
   $java = true,
   $java_version = '8', # 7, 8
   $java_ensure = 'latest', # present, latest, absent
