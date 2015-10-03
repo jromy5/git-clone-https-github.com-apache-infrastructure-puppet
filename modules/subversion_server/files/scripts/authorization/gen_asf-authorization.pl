e572fed62595defac569357b74a302302d503c5d
Geoffrey Corey <coreyg@apache.org>
2015-10-03 21:51:48 +0200

    point svn01 gen script to local ldap

--- modules/subversion_server/files/scripts/authorization/gen_asf-authorization.pl
+++ modules/subversion_server/files/scripts/authorization/gen_asf-authorization.pl
@@ -21,7 +21,7 @@ use Net::LDAP::Constant qw(
   LDAP_SYNC_REFRESH_AND_PERSIST
   LDAP_SUCCESS );
 
-my $LDAPHOST  = "ldaps://ldap1-us-west.apache.org";
+my $LDAPHOST  = "ldaps://ldap3-us-west.apache.org";
 my $debug         = 1;
 my ( $cookie, $ldap, %watchlist );
 
