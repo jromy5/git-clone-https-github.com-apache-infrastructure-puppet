4cce2d920b1de0260e26d18b58eab683d04eb095
Geoffrey Corey <coreyg@apache.org>
2015-10-08 09:13:09 -0700

    add reporting.cloudstack.a.o ssl key deployment

---
+++ modules/ssl/manifests/name/reporting_cloudstack_apache_org.pp
@@ -0,0 +1,60 @@
+#/etc/puppet/modules/manifests/ssl/name/reporting_cloudstack_apache_org.pp
+
+class ssl::name::reporting_cloudstack_apache_org (
+
+  $sslcertcontents             = '',
+  $sslcertname                 = 'reporting.cloudstack.apache.org.crt',
+  $sslchaincontent             = '',
+  $sslchainname                = 'reporting.cloudstack.apache.org.chain',
+  $sslkeycontents              = '',
+  $sslkeyname                  = 'reporting.cloudstack.apache.org.key',
+  $sslcombinedcontents         = '',
+  $sslcombinedname             = 'reporting.cloudstack.apache.org-combined.cert',
+  $sslrootdir                  = '/etc/ssl',
+  $sslrootdirgroup             = 'root',
+  $sslrootdirowner             = 'root',
+  $sslrootdirumask             = '0755',
+) {
+
+  file {
+    $sslrootdir:
+      ensure => directory,
+      group  => $sslrootdirgroup,
+      owner  => $sslrootdirowner,
+      mode   => $sslrootdirumask;
+    "${sslrootdir}/certs":
+      ensure => directory,
+      group  => $sslrootdirgroup,
+      owner  => $sslrootdirowner,
+      mode   => '0755';
+    "${sslrootdir}/private":
+      ensure => directory,
+      group  => $sslrootdirgroup,
+      owner  => $sslrootdirowner,
+      mode   => '0700';
+    "${sslrootdir}/certs/${sslcertname}":
+      ensure  => present,
+      require => File[$sslrootdir],
+      content => $sslcertcontents,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+    "${sslrootdir}/private/${sslkeyname}":
+      ensure  => present,
+      require => File[$sslrootdir],
+      content => $sslkeycontents,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+    "${sslrootdir}/certs/${sslchainname}":
+      ensure  => present,
+      require => File[$sslrootdir],
+      content => $sslchaincontent,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+    "${sslrootdir}/private/${sslcombinedname}":
+      ensure  => present,
+      require => File[$sslrootdir],
+      content => $sslcombinedcontents,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+  }
+}
