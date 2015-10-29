23e4dbf92f7ee6aff6a99facd5660e00400e0851
Geoffrey Corey <coreyg@apache.org>
2015-10-28 17:48:01 -0700

    fix bash syntax

--- modules/rsync_mirror/files/kill_stale_rsync.sh
+++ modules/rsync_mirror/files/kill_stale_rsync.sh
@@ -4,7 +4,7 @@
 to_kill=$(/usr/local/bin/rsync_hang.pl | tail -1)
 
 # Check if list of PIDs iis empty, kill if not empty
-if [ ! -z $to_kill ]; then
+if [ ! -z "$to_kill" ]; then
     echo "Killing stale rsync PIDs $to_kill" | logger
     kill -15 $to_kill
 fi
