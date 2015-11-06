06a70bd1d6c6cd4e662a036b56dc8eb8a03ee11a
Daniel Takamori <pono@apache.org>
2015-11-06 10:28:12 -0800

    inital manifest for buildbot_slave

---
+++ modules/buildbot_slaves/manifests/init.pp
@@ -0,0 +1,49 @@
+##/etc/puppet/modules/buildbot_slave/manifests/init.pp
+
+class buildbot_slave (
+
+  $uid                           = 8996,
+  $gid                           = 8996,
+  $group_present                 = 'present',
+  $groupname                     = 'buildslave',
+  $groups                        = [],
+  $shell                         = '/bin/bash',
+  $user_present                  = 'present',
+  $username                      = 'buildslave',
+
+
+# buildbot specific
+
+  user {
+    $username:
+      ensure     => $user_present,
+      name       => $username,
+      home       => "/x1/${username}",
+      shell      => $shell,
+      uid        => $uid,
+      gid        => $groupname,
+      groups     => $groups,
+      managehome => true,
+      require    => Group[$groupname],
+  }
+
+  group {
+    $groupname:
+      ensure => $group_present,
+      name   => $groupname,
+      gid    => $gid,
+}
+
+  apt::source { 'utopic':
+    location => 'http://us.archive.ubuntu.com/ubuntu/',
+    release  => 'utopic',
+    repos    => 'main',
+  }
+
+  apt::source { 'utopic-updates':
+    location => 'http://us.archive.ubuntu.com/ubuntu/',
+    release  => 'utopic-updates',
+    repos    => 'main',
+  }
+
+}
