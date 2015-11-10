391a139af29792084d346c395d72bd2f666e539a
Sebb <sebb@apache.org>
2015-11-10 19:33:28 +0000

    Fix cron syntax

--- modules/projects_pvm_asf/manifests/init.pp
+++ modules/projects_pvm_asf/manifests/init.pp
@@ -26,8 +26,8 @@ class projects_pvm_asf (
   # Check in any updated data/releases files
   # Append stdout to  /var/log/www-data-root/svnreleases_$(date "+%Y-%m").log
     'rao_releases_ci':
-      minute  => '45',
-      hour    => '0,6,12,18',
+      minute  => 45,
+      hour    => [0, 6, 12, 18],
       user    => 'root',
       require => File['/var/log/www-data-root'],
       command => 'cd /var/www/reporter.apache.org/data/releases && sudo -n -u www-data svn ci -m "updating report releases data" --username projects_role --password `cat /root/.rolepwd` --non-interactive >>/var/log/www-data-root/svnreleases_$(date "+%Y-%m").log';
@@ -36,8 +36,8 @@ class projects_pvm_asf (
   # Check in any updated data/history files
   # These are copied extracts of the files created by parsepmcs.py which is run at 00 min 4,12,20 hours
     'rao_history_ci':
-      minute  => '10',
-      hour    => '4,12,20',
+      minute  => 10,
+      hour    => [4, 12, 20],
       user    => 'root',
       require => File['/var/log/www-data-root'],
       command => 'cd /var/www/reporter.apache.org/data/history && sudo -n -u www-data svn ci -m "updating report releases data" --username projects_role --password `cat /root/.rolepwd` --non-interactive >>/var/log/www-data-root/svnhistory_$(date "+%Y-%m").log';
@@ -45,8 +45,8 @@ class projects_pvm_asf (
   # Check in any updated/new json files under projects.apache.org
   # The "svn add" job run under www-data is run at 04:10
     'pao_json_ci':
-      minute  => '20',
-      hour    => '4',
+      minute  => 20,
+      hour    => 4,
       user    => 'root',
       require => File['/var/log/www-data-root'],
       command => 'cd /var/www/projects.apache.org/site/json && sudo -n -u www-data svn ci -m "updating projects data" --username projects_role --password `cat /root/.rolepwd` --non-interactive >>/var/log/www-data-root/svnjson_$(date "+%Y-%m").log';
