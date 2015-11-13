9bf49e0035625940338ee7c8f4a62c5e97ba564e
humbedooh <humbedooh@apache.org>
2015-11-13 17:16:44 +0100

    only allow allhost holders to save search tabs

--- modules/kibana_asf/files/frombrowser.lua
+++ modules/kibana_asf/files/frombrowser.lua
@@ -18,6 +18,8 @@ end
 -- Get which user has authed this request
 local user = os.getenv('REMOTE_USER')
 local kint = os.getenv('REQUEST_URI'):match("^/kibana-int/") and true or false
+local rm = os.getenv('REQUEST_METHOD')
+
 
 -- Query LDAP for host records
 local hosts = {}
@@ -64,21 +66,26 @@ if p then
     end
 end
 
--- Read JSON data from stdin
-local data = io.stdin:read("*a")
-local valid, json = pcall(function() return JSON:decode(data) end)
-    
--- If the input contains a query, then mangle it...
-if valid and json then
-    -- If user doesn't have access to "*", then inject a host requirement into the query
-    if not allHosts or kint then
-        local what = " AND (" .. table.concat(hosts, " OR ") .. ")"
-        inject(json, what)
-    end
-    local output = JSON:encode(json)
-    print(output) -- Send converted data down the chain
-    
--- If there's no query, just return what was sent
+-- only allow put/delete for allHost accessers
+if not allHosts and (rm == "PUT" or rm == "DELETE") then
+    print("{}")
 else
-    print(data)
+    -- Read JSON data from stdin
+    local data = io.stdin:read("*a")
+    local valid, json = pcall(function() return JSON:decode(data) end)
+    
+    -- If the input contains a query, then mangle it...
+    if valid and json then
+        -- If user doesn't have access to "*", then inject a host requirement into the query
+        if not (allHosts or kint) then
+            local what = " AND (" .. table.concat(hosts, " OR ") .. ")"
+            inject(json, what)
+        else 
+            local output = JSON:encode(json)
+            print(output) -- Send converted data down the chain
+        end
+    -- If there's no query, just return what was sent
+    else
+        print(data)
+    end
 end
