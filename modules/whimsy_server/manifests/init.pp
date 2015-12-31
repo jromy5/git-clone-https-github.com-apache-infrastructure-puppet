92b32b19f8658e2bc65f31900a158ea15859ec62
Sam Ruby <rubys@intertwingly.net>
2015-12-31 12:29:27 -0500

    add some content to whimsy-vm2

---
+++ modules/whimsy_server/manifests/init.pp
@@ -0,0 +1,14 @@
+#/etc/puppet/modules/whimsy_server/manifests/init.pp
+
+
+class whimsy_server (
+
+) {
+
+  vcsrepo { '/srv/whimsy':
+    ensure => latest,
+    provider => git,
+    source => 'https://github.com/apache/whimsy.git'
+  }
+
+}
