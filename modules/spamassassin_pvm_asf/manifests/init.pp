511e6900114516bf4a7de3d22f3e74f7be166a05
Chris Lambertus <clambertus@gmail.com>
2016-01-07 08:26:53 -0800

    initial spamassassin VM (replacement for spamassassin-vm on eirene)

---
+++ modules/spamassassin_pvm_asf/manifests/init.pp
@@ -0,0 +1,27 @@
+# /etc/puppet/modules/spamassassin_pvm_asf/manifests/init.pp
+
+class spamassassin_pvm_asf (
+
+  $required_packages = ['joe' , 'ant' , 'unzip' , 'tomcat7' , 'tcsh'],
+  $java = true,
+  $java_version = '8', # 7, 8
+  $java_ensure = 'latest', # present, latest, absent
+
+){
+
+# install required packages:
+  package {
+    $required_packages:
+      ensure => 'present',
+  }
+
+  if $java {
+    class { 'oraclejava::install':
+      ensure  => $java_ensure,
+      version => $java_version,
+    }
+  }
+
+}
+
+
