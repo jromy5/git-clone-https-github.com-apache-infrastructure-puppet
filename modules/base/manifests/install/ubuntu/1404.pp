6c7bc12577ef6aeed83cfca838eb679a9af2b506
Geoffrey Corey <coreyg@apache.org>
2016-01-13 13:45:39 -0800

    remove file removal, make it a cron, make puppet not so noisy

--- modules/base/manifests/install/ubuntu/1404.pp
+++ modules/base/manifests/install/ubuntu/1404.pp
@@ -23,17 +23,20 @@ class base::install::ubuntu::1404 (
     '/etc/logrotate.d/rsyslog':
       ensure => present,
       source => 'puppet:///modules/base/logrotate-rsyslog';
-    '/var/lib/puppet/state/state.yaml':
-      ensure => absent;
   }->
 
-  cron { 'purge_old_kernels':
-    ensure      => present,
-    command     => '/bin/sh /root/purge_old_kernels -y > /dev/null',
-    environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin", # lint:ignore:double_quoted_strings
-    minute      => '10',
-    hour        => '0',
-    weekday     => '0',
-    require     => File['/root/purge_old_kernels'],
+  cron {
+    'purge_old_kernels':
+      ensure      => present,
+      command     => '/bin/sh /root/purge_old_kernels -y > /dev/null',
+      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin", # lint:ignore:double_quoted_strings
+      minute      => '10',
+      hour        => '0',
+      weekday     => '0',
+      require     => File['/root/purge_old_kernels'];
+    'delete state.yaml':
+      ensure  => present,
+      command => 'rm -f /var/lib/puppet/state/state.yaml',
+      hour    => '0';
   }
 }
