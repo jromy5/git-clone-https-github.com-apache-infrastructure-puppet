944498b091b8b000d0dad8542b623a83f034a2e1
Chris Lambertus <clambertus@gmail.com>
2016-02-21 08:32:44 -0800

    bai ssl/stunnel configuration

--- modules/ssl/manifests/name/bai_stunnel.pp
+++ modules/ssl/manifests/name/bai_stunnel.pp
@@ -2,36 +2,64 @@
 
 class ssl::name::bai_stunnel (
 
-  $sslcertcontents     = '',
-  $sslcertname         = 'bai.apache.org.crt',
-  $sslchaincontent     = '',
-  $sslchainname        = 'bai.apache.org.ca',
-  $sslkeycontents      = '',
-  $sslkeyname          = 'bai.apache.org.key',
-  $sslcombinedcontents = '',
-  $sslcombinedname     = '',
-  $sslrootdir          = '/etc/ssl',
-  $sslrootdirgroup     = 'root',
-  $sslrootdirowner     = 'root',
-  $sslrootdirumask     = '0755',
+  $sslcrlcontents       = '',
+  $sslcrlname           = 'bai.apache.org.crl',
+  $sslcertcontents      = '',
+  $sslcertname          = 'bai.apache.org.crt',
+  $sslchaincontents     = '',
+  $sslchainname         = 'bai.apache.org.ca',
+  $sslkeycontents       = '',
+  $sslkeyname           = 'bai.apache.org.key',
+  $sslcombinedcontents  = '',
+  $sslcombinedname      = '',
+  $sslrootdir           = '/etc/ssl',
+  $sslrootdirgroup      = 'root',
+  $sslrootdirowner      = 'root',
+  $sslrootdirumask      = '0755',
 ) {
+
+  if !defined(File["${sslrootdir}"]) {
+    file {
+     "${sslrootdir}":
+       ensure => directory,
+       group  => 'root',
+       owner  => 'root',
+       mode   => '0755';
+    "${sslrootdir}/certs":
+      ensure => directory,
+      group  => 'root',
+      owner  => 'root',
+      mode   => '0755';
+    "${sslrootdir}/private":
+      ensure => directory,
+      group  => 'root',
+      owner  => 'root',
+      mode   => '0700';
+    }
+  }
 
   file {
     "${sslrootdir}/certs/${sslcertname}":
       ensure   => present,
-      require => File[$sslrootdir],
+      require => File["$sslrootdir/certs"],
       content => $sslcertcontents,
+      owner   => $sslrootdirowner,
+      group   => $sslrootdirgroup;
+    "${sslrootdir}/private/${sslcrlname}":
+      ensure  => present,
+      require => File["$sslrootdir/private"],
+      content => $sslcrlcontents,
       owner   => $sslrootdirowner,
       group   => $sslrootdirgroup;
     "${sslrootdir}/private/${sslkeyname}":
       ensure  => present,
-      require => File[$sslrootdir],
+      require => File["$sslrootdir/private"],
       content => $sslkeycontents,
       owner   => $sslrootdirowner,
       group   => $sslrootdirgroup;
     "${sslrootdir}/certs/${sslchainname}":
       ensure  => present,
-      require => File[$sslrootdir],
+      require => File["$sslrootdir/certs"],
       content => $sslchaincontents,
       owner   => $sslrootdirowner,
       group   => $sslrootdirgroup;
