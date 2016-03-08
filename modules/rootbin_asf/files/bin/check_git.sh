31beec47b2dbcfc0aa1f604dcf2a2b5eea7e2bbb
Geoffrey Corey <coreyg@apache.org>
2016-03-08 09:51:08 -0800

    add some documentation

--- modules/rootbin_asf/files/bin/check_git.sh
+++ modules/rootbin_asf/files/bin/check_git.sh
@@ -9,17 +9,19 @@ git ls-remote git://$GIT_URL/$GIT_REPO > /dev/null 2>&1
 # If command does not exit successfully, restart the daemon
 if [ $? -ne 0 ]; then
     sleep 5
+    # Run again just to double check it isn't responding
     git ls-remote git://$GIT_URL/$GIT_REPO > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         logger "Restarting git-daemon"
-        #echo "Restarting git-daemon" | mail -s "git check script" coreyg@apache.org
         service git-daemon stop >/dev/null 2>&1
         sleep 1
+        # Get list of stale git connections to kill
         for i in $(ps aux --sort=lstart | grep git | egrep "daemon|upload" | awk '{ print $2 }'); 
         do 
             kill -9 $i
         done
         sleep 1
+        logger "Starting git-daemon again"
         service git-daemon start >/dev/null 2>&1
     fi
 fi
