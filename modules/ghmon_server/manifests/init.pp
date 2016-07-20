8fea8223f398a77e67173c1b0c1b06b80fe576b0
Sam Ruby <rubys@intertwingly.net>
2016-07-20 10:49:51 -0400

    configure certbot

---
+++ modules/ghmon_server/manifests/init.pp
@@ -0,0 +1,26 @@
+#/etc/puppet/modules/whimsy_server/manifests/init.pp
+
+
+class ghmon_server {
+
+  ####################################################################
+  #                      certbot / letsencrypt                       #
+  ####################################################################
+
+  exec { 'Download certbot':
+    command => '/usr/bin/wget -q https://dl.eff.org/certbot-auto -O /usr/local/bin/certbot-auto',
+    creates => '/usr/local/bin/certbot-auto',
+  } ->
+
+  file { '/usr/local/bin/certbot-auto':
+    mode => 0755
+  }
+
+  cron { 'certbot renew':
+    ensure  => present,
+    command => "/usr/local/bin/certbot-auto renew --quiet",
+    user    => root,
+    hour    => [2, 14],
+    minute  => '43'
+  }
+}
