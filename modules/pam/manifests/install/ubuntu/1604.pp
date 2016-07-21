efd09badfa55e4831ee71e9129d757ffd7cc8eaf
Chris Lambertus <clambertus@gmail.com>
2016-07-20 18:04:20 -0700

    add 1604 support to puppet

---
+++ modules/pam/manifests/install/ubuntu/1604.pp
@@ -0,0 +1,14 @@
+#/etc/puppet/modules/pam/install/ubuntu/1604.pp
+
+class pam::install::ubuntu::1604 (
+) {
+
+  file { '/etc/pam.d/':
+    ensure  => present,
+    source  => "puppet:///modules/pam/${::asfosname}/${::asfosrelease}",
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+}
