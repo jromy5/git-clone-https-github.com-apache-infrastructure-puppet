f62df6aa75e5ca45964c7e567d51428e814c334e
gmcdonald <gmcdonald@apache.org>
2016-07-25 20:07:23 +1000

    zfs_on_ubuntu module lint fixes only (ignore 140+ char lines)

--- modules/zfs_on_ubuntu/manifests/init.pp
+++ modules/zfs_on_ubuntu/manifests/init.pp
@@ -3,9 +3,9 @@
 class zfs_on_ubuntu {
 
   $packages_pre = [
-    "linux-headers-${kernelrelease}",
+    "linux-headers-${::kernelrelease}",
   ]
-    
+
   $packages_zfs = [
     'zfs-dkms',
     'ubuntu-zfs'
@@ -15,11 +15,11 @@ class zfs_on_ubuntu {
 
   package { $packages_pre:
     ensure  => installed,
-  } ~> 
+  } ~>
   package { $packages_zfs:
     ensure  => installed,
     require => Apt::Ppa['ppa:zfs-native/stable'],
-  } ~> 
+  } ~>
   exec { 'modprobe zfs':
     command => '/sbin/modprobe zfs'
   }
