f65c13849d0bcab0d90916988f3ba3f530d10c34
gmcdonald <gmcdonald@apache.org>
2016-07-26 10:40:16 +1000

    nfs_asf module lint fixes only (ignore 140+ char lines)

--- modules/nfs_asf/manifests/init.pp
+++ modules/nfs_asf/manifests/init.pp
@@ -1,11 +1,13 @@
 # create_resources for nfs defined types
 
+# server nfs class
 class nfs_asf::server {
   $exports = hiera_hash('nfs::server::export',{})
   create_resources(nfs::server::export, $exports)
   contain nfs::server
 }
 
+# client nfs class
 class nfs_asf::client {
   $mounts = hiera_hash('nfs::client::mount',{})
   create_resources(nfs::client::mount, $mounts)
