120d5547b7baa8fb5e5e967b8832d373cb6ac5df
Daniel Takamori <pono@apache.org>
2016-09-03 14:14:15 -0600

    typo in name

--- modules/buildbot_slave/manifests/cpan.pp
+++ modules/buildbot_slave/manifests/cpan.pp
@@ -1,11 +1,11 @@
-#/etc/puppet/modules/build_slaves/manifests/jenkins.pp
+#/etc/puppet/modules/buildbot_slave/manifests/cpan.pp
 
 include cpan
 
 # jenkins class for the build slaves.
 class buildbot_slave::cpan (
   
-  $modules = ['Archive::Zip', 'LWP::UserAgent', 'LWP::Protocol', 'XML::Parser', 'LWP::Protocol::https'],
+  $modules,
 
 ) {
 
