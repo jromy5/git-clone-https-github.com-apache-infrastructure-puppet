70e898c9cb0c54e3cc2be3137e28ce59dd21942d
Daniel Takamori <pono@apache.org>
2016-10-04 21:08:25 +0000

    add crons from puppet2 to svnqavm_pvm_asf

--- modules/svnqavm_pvm_asf/manifests/init.pp
+++ modules/svnqavm_pvm_asf/manifests/init.pp
@@ -32,4 +32,27 @@ class svnqavm_pvm_asf (
     system     => true,
   }
 
+  cron {
+    wayita-cron:
+      command => "/home/wayita/bin/wayita-cron.sh",
+      user    => "wayita",
+      hour    => 14,
+      minute  => 23;
+    # Run backport merges
+    backport-cron:
+      command     => 'for i in 1.6.x 1.7.x 1.8.x 1.9.x ; do cd && cd src/svn/$i && $SVN up -q --non-interactive && YES=1 MAY_COMMIT=1 ../trunk/tools/dist/backport.pl; done',
+      user        => "svnsvn",
+      hour        => 4,
+      minute      => 0,
+      environment => "SVN=/usr/local/svn-current/bin/svn";
+    # Log the revision of backport.pl in use.
+    # (There's no log rotation since this will use about 2KB a year.)
+    backport-version-log:
+      command     => '(date +\%Y\%m\%d: ; ${SVNVERSION} src/svn/trunk/tools/dist/backport.pl) >> ~/live-version.log',
+      user        => "svnsvn",
+      hour        => 4,
+      minute      => 0,
+      environment => "SVNVERSION=/usr/local/svn-current/bin/svnversion";
+  }
+
 }
