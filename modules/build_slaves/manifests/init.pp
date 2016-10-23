f18d5ab8153b6e5c9479e875df2838f4b67d8a09
Chris Lambertus <cml@apache.org>
2016-10-23 00:19:44 -0700

    remove errant node* packages as they are deprecated with nodejs6. add the v6 repos in a way that makes sense for both 1404 and 1604 right now.

--- modules/build_slaves/manifests/init.pp
+++ modules/build_slaves/manifests/init.pp
@@ -7,6 +7,12 @@ class build_slaves (
   class { "build_slaves::install::${::asfosname}::${::asfosrelease}":
   }
 
+  exec { 'Add nodesource-6 sources':
+    command => 'if [ ! -f /etc/apt/sources.list.d/nodesource.list ] ; then curl https://deb.nodesource.com/setup_6.x ; fi | bash -',
+    creates => '/etc/apt/sources.list.d/nodesource.list',
+    path    => ['/usr/bin', '/bin', '/usr/sbin']
+  }
+
   package {
     $distro_packages:
       ensure => installed,
