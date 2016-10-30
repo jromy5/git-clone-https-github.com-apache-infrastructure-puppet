d92330266fdbdf480176437964651ff2261830d8
Daniel Takamori <pono@apache.org>
2016-10-29 20:52:17 -0600

    remove nodesource.list since nodesource-6.list exists

--- modules/build_slaves/manifests/init.pp
+++ modules/build_slaves/manifests/init.pp
@@ -23,6 +23,9 @@ class build_slaves (
   } -> 
   exec { 'delete dupe sources.list.d files':
     command => "/bin/rm -f /etc/apt/sources.list.d/packages_apache_org_asf_internal.list /etc/apt/sources.list.d/get_docker_io_ubuntu.list /etc/apt/sources.list.d/dell.list",
+  } ->
+  exec {'delete dupe nodesource repo':
+    command => "/bin/rm -f /etc/apt/sources.list.d/nodesource.list",
   }
 
 ## end temporary block
