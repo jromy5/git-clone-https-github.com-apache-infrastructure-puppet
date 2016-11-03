d2af645c59eda0f56d295b92742529449b1fc75b
gmcdonald <gmcdonald@apache.org>
2016-11-03 18:35:44 +1100

    update Clover for Ant to 4.1.2. Repackage for 1604

--- modules/build_slaves/manifests/jenkins.pp
+++ modules/build_slaves/manifests/jenkins.pp
@@ -11,7 +11,7 @@ class build_slaves::jenkins (
   $tools = ['ant','clover','findbugs','forrest','java','maven', 'jiracli'],
   $ant_old = ['apache-ant-1.9.4'],
   $ant = ['apache-ant-1.8.4','apache-ant-1.9.7'],
-  $clover = ['clover-ant-4.1.1'],
+  $clover = ['clover-ant-4.1.2'],
   $findbugs = ['findbugs-2.0.3'],
   $forrest = ['apache-forrest-0.9'],
   $jiracli = ['jira-cli-2.1.0'],
