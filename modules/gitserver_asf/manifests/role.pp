f56fe254764fc0c6520adc3be4ddb2b86d27e482
Daniel Takamori <pono@apache.org>
2016-11-18 09:59:38 +0000

    update mergebot creds and gitserver_asf::role

---
+++ modules/gitserver_asf/manifests/role.pp
@@ -0,0 +1,10 @@
+#/etc/puppet/modules/gitserver_asf/manifests/role.pp
+
+
+class gitserver_asf::role (
+  $role_account_username = '',
+  $role_account_password = '',
+  $role_account_token = '',
+) {
+
+}
