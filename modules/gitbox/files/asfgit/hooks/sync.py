f36e24e7d57420ebf3b4006c447c1eb9acdbd591
Daniel Gruno <humbedooh@apache.org>
2016-11-25 15:44:15 +0100

    fix paths for sync.py

--- modules/gitbox/files/asfgit/hooks/sync.py
+++ modules/gitbox/files/asfgit/hooks/sync.py
@@ -11,15 +11,15 @@ import subprocess, os, time
 
 def main():
     ghurl = "git@github:apache/%s.git" % cfg.repo_name
-    os.chdir("/x1/git/repos/asf/%s.git" % cfg.repo_name)
+    os.chdir("/x1/repos/asf/%s.git" % cfg.repo_name)
     try:
        subprocess.check_call(["git", "push", "--all", ghurl])
        try:
-           os.unlink("/x1/git/git-dual/broken/%s.txt" % cfg.repo_name)
+           os.unlink("/x1/gitbox/broken/%s.txt" % cfg.repo_name)
        except:
            pass
     except Exception as err:
-       with open("/x1/git/git-dual/broken/%s.txt" % cfg.repo_name, "w") as f:
+       with open("/x1/git/gitbox/broken/%s.txt" % cfg.repo_name, "w") as f:
            f.write("BROKEN AT %s\n" % time.strftime("%c"))
            f.close()
        log.exception(err)
