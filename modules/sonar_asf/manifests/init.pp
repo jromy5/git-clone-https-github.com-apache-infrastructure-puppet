a8226fb8206ebecab5cd28b0326d4b6e33226fd2
gmcdonald <gmcdonald@apache.org>
2016-12-10 14:00:50 +1100

    Add Sonar analysis module and initial yaml config

---
+++ modules/sonar_asf/manifests/init.pp
@@ -0,0 +1,14 @@
+#/etc/puppet/modules/sonar_asf/manifests/init.pp
+
+class sonar_asf (
+
+  $required_packages             = ['tomcat8'],
+){
+
+# install required packages:
+  package {
+    $required_packages:
+      ensure => 'present',
+  }
+
+# Sonar specific TBD
