f268b68c3e6a93afa35542badaa65de744557ccd
Chris Lambertus <cml@apache.org>
2016-12-23 09:16:37 -0800

    temp commit - wip

--- modules/reviews_asf/manifests/init.pp
+++ modules/reviews_asf/manifests/init.pp
@@ -1,10 +1,19 @@
+# deploy reviewboard
+
 class reviews_asf {
 
-  class { 'memcached':
-    max_memory  => '10%',
-    listen_ip   => '127.0.0.1',
-    tcp_port    => '11211',
+  $reviews_packages = [
+    'patch',
+  ]
+
+  package { $reviews_packages:
+    ensure => present,
   }
 
-  
+  class { 'memcached':
+    max_memory => '10%',
+    listen_ip  => '127.0.0.1',
+    tcp_port   => '11211',
+  }
+
 }
