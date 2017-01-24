6449d7cd0c51b98bfcc332786901fe157e3b5406
Daniel Takamori <pono@apache.org>
2017-01-23 17:25:03 -0700

    slaves != slave

--- modules/buildbot_slave/manifests/github.pp
+++ modules/buildbot_slave/manifests/github.pp
@@ -11,19 +11,19 @@ class buildbot_slave::github (
 
   file {
 
-     "/home/${buildbot_slaves::username}/.ssh/id_rsa_github":
-      require => File["/home/${buildbot_slaves::username}/.ssh"],
-      path    => "/home/${buildbot_slaves::username}/.ssh/id_rsa_github",
-      owner   => $buildbot_slaves::username,
-      group   => $buildbot_slaves::groupname,
+     "/home/${buildbot_slave::username}/.ssh/id_rsa_github":
+      require => File["/home/${buildbot_slave::username}/.ssh"],
+      path    => "/home/${buildbot_slave::username}/.ssh/id_rsa_github",
+      owner   => $buildbot_slave::username,
+      group   => $buildbot_slave::groupname,
       mode    => '0600',
       content => template('buildbot_slave/ssh/id_rsa_github.erb');
 
-    "/home/${buildbot_slaves::username}/.ssh/id_rsa_github.pub":
-      require => File["/home/${buildbot_slaves::username}/.ssh"],
-      path    => "/home/${buildbot_slaves::username}/.ssh/id_rsa_github.pub",
-      owner   => $buildbot_slaves::username,
-      group   => $buildbot_slaves::groupname,
+    "/home/${buildbot_slave::username}/.ssh/id_rsa_github.pub":
+      require => File["/home/${buildbot_slave::username}/.ssh"],
+      path    => "/home/${buildbot_slave::username}/.ssh/id_rsa_github.pub",
+      owner   => $buildbot_slave::username,
+      group   => $buildbot_slave::groupname,
       mode    => '0644',
       content => template('buildbot_slave/ssh/id_rsa_github.pub.erb');
 
