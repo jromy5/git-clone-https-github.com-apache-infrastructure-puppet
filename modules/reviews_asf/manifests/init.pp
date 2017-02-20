bdc5526446e87694687fe7318403a2c1ea22670f
Chris Lambertus <cml@apache.org>
2017-02-19 22:27:14 -0800

    initial reviewboard commit - add database, machine configs, vhosts, and script to install RB. NB: this commit also reduces the retained binlogs on mysql3 to reduce space usage.

--- modules/reviews_asf/manifests/init.pp
+++ modules/reviews_asf/manifests/init.pp
@@ -1,19 +1,74 @@
 # deploy reviewboard
 
-class reviews_asf {
+class reviews_asf  (
+  $dbuser     = '',
+  $dbpass     = '',
+  $dbname     = '',
+  $dbtype     = 'mysql',
+  $dbhost     = '',
+  $secret_key = '',
+  $rbhome     = '/var/www/reviews.apache.org',
+  $rbhost     = 'reviews.apache.org',
+  $rbuser     = 'reviewboard',
+  $rbgroup    = 'reviewboard',
+  $rbuid      = '20000',
+  $rbgid      = '20000',
+) {
 
-  $reviews_packages = [
-    'patch',
-  ]
-
-  package { $reviews_packages:
-    ensure => present,
+  user {
+    $rbuser:
+      ensure     => 'present',
+      name       => $rbuser,
+      home       => "/home/${rbuser}",
+      managehome => true,
+      uid        => $rbuid,
+      gid        => $rbgid,
+      require    => Group[$rbgroup],
   }
 
-  class { 'memcached':
-    max_memory => '10%',
-    listen_ip  => '127.0.0.1',
-    tcp_port   => '11211',
+  group {
+    $rbgroup:
+      name => $rbgroup,
+      gid  => $rbgid,
   }
 
+  exec { 'rbsite-install':
+    command => '/usr/local/bin/rbsite-install.sh',
+    creates => "${rbhome}/.installed",
+    onlyif  => "/usr/bin/test ! -f ${rbhome}/.installed",
+    require => File['/usr/local/bin/rbsite-install.sh'],
+  }
+
+  file {
+    '/usr/local/bin/rbsite-install.sh':
+      ensure  => 'present',
+      content => template('reviews_asf/rbsite-install.sh.erb'),
+      mode    => '0750';
+    "${rbhome}/conf/settings_local.py":
+      ensure  => 'present',
+      content => template('reviews_asf/settings_local.py.erb'),
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0640',
+      require => Exec['rbsite-install'];
+    "${rbhome}/data":
+      ensure  => 'directory',
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0750',
+      require => Exec['rbsite-install'];
+    "${rbhome}/htdocs/media/ext":
+      ensure  => 'directory',
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0750',
+      require => Exec['rbsite-install'];
+    "${rbhome}/htdocs/static/ext":
+      ensure  => 'directory',
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0750',
+      require => Exec['rbsite-install'];
+
+  }
 }
