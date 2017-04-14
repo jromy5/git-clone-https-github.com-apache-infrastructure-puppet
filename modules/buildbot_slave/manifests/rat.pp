deb6cc2ec81c63a4b4b5d0920065915177075b2c
Daniel Takamori <pono@apache.org>
2017-04-13 20:58:01 -0600

    change rat module to use static files rather than erbs

--- modules/buildbot_slave/manifests/rat.pp
+++ modules/buildbot_slave/manifests/rat.pp
@@ -2,19 +2,9 @@
 
   # class for the buildbot slaves making rat reports available.
   class buildbot_slave::rat (
-  # $project_name: Project name without Apache prefix: example: 'OpenOffice' : default empty
-  $project_name  = '',
-  # $src_dir: Matches the build name: example: 'openoffice-nightly-rat' : default current dir.
-  $src_dir       = '.',
-  # $build_dir: Default is 'build' and appends to $src_dir
-  $build_dir     = 'build',
-  # $build_version: Whatever version of the RAT tool we are using.
-  $build_version = '0.12',
-  # $report_file: the xml output of the rat report (to be uploaded to master) : default 'rat-output.xml' 
-  $report_file   = 'rat-output.xml',
-  # $rat_excludes: A file in the source checkout that contains a list of patterns to exclude from the RAT check.
-  # $rat_excludes: example: /path/to/rat-excludes : $src_dir is prepended automatically.
-  $rat_excludes  = '',
+
+  $projects = [],
+
 ) {
 
 require stdlib
@@ -29,22 +19,23 @@ require buildbot_slave
   $install_dir        = '/home/buildslave/'
 
   file {
-    '/home/buildslave/slave/rat-buildfiles/rat.xml':
-    ensure  => present,
-    path    => '/home/buildslave/slave/rat-buildfiles/rat.xml',
-    owner   => $buildbot_slave::username,
-    group   => $buildbot_slave::groupname,
-    mode    => '0640',
-    content => template('buildbot_slave/rat.xml.erb'),
-    require => [Package['ant'],File['/home/buildslave/slave/rat-buildfiles'],Group[$buildbot_slave::groupname]];
-
     '/home/buildslave/slave/rat-buildfiles':
-    ensure  => 'directory',
-    owner   => $buildbot_slave::username,
-    group   => $buildbot_slave::groupname,
-    mode    => '0755',
-    require => [Group[$buildbot_slave::groupname]];
+      ensure  => 'directory',
+      owner   => $buildbot_slave::username,
+      group   => $buildbot_slave::groupname,
+      mode    => '0755',
+      require => [Group[$buildbot_slave::groupname]];
+  }
 
+  define buildbot_slave::rats ($project = $title) {
+    file {"/home/buildslave/slave/rat-buildfiles//${project}.xml":
+      ensure  => present,
+      owner   => $buildbot_slave::username,
+      group   => $buildbot_slave::groupname,
+      mode    => '0640',
+      source  => 'puppet:///modules/buildbot_slave/${project}.xml',
+      require => [Package['ant'],File['/home/buildslave/slave/rat-buildfiles'],Group[$buildbot_slave::groupname]];
+    }
   }
 
 # download RAT
@@ -71,5 +62,7 @@ require buildbot_slave
       require => [File[$downloaded_tarball]],
       onlyif  => "/usr/bin/test -d ${install_dir}",
   }
+
+  buildbot_slave::rats { $projects: }
 
 }
