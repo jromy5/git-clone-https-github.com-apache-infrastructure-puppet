4f83e393242b8e75580b3a7b354552005297afd9
gmcdonald <gmcdonald@apache.org>
2017-04-15 16:50:06 +1000

    Improve readability of output for easier debugging

--- modules/buildbot_asf/files/convert-xml-to-html.sh
+++ modules/buildbot_asf/files/convert-xml-to-html.sh
@@ -4,5 +4,5 @@
 ### See buildbot_asf Module ###
 
 cd /x1/buildmaster/master1/public_html/projects
-for F in `find . -mindepth 2 -name rat-output.xml`; do java -jar /x1/buildmaster/saxon9/saxonb9/saxon9.jar -t -s:$F -xsl:/x1/buildmaster/master1/rat-output.xsl -o:`dirname $F`/rat-output.html; done
+for F in `find . -mindepth 2 -name rat-output.xml`; do echo "Building $F"; echo "----------------------------------------"; java -jar /x1/buildmaster/saxon9/saxonb9/saxon9.jar -t -s:$F -xsl:/x1/buildmaster/master1/rat-output.xsl -o:`dirname $F`/rat-output.html; echo " "; done
 # find . -mindepth 2 -name rat-output.xml -exec rm -f {} \; ## Leave the xml file whilst testing.
