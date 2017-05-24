cc5f5eccb0fbfa0f7c7bd9231d5d5fcc349804b1
Sam Ruby <rubys@intertwingly.net>
2017-05-24 09:29:38 -0400

    allow LDAP attribute to be specified in authorization templates
    look for ";attr=" within {ldap:} sections, and extract attribute name
    if this string is present; otherwise default to memberUid,member for
    backwards compatibility.
    
    A single example of this new syntax is included in this commit.
    
    The plans are to shortly make use of this to move PMC authorizations to
    an owner attribute.

--- modules/subversion_server/files/scripts/authorization/gen_asf-authorization.pl
+++ modules/subversion_server/files/scripts/authorization/gen_asf-authorization.pl
@@ -93,15 +93,17 @@ sub rebuild {
             die "Uh-oh: found '@lines', expected one line; at '$_'" unless @lines == 1;
             $_ = $lines[0];
         }
-        if ($_ !~ m/^#/ && $_ =~ m/{ldap:(cn=.+)}/ ) {
+        if ( $_ !~ m/^#/ && $_ =~ m/{ldap:(cn=.+?)(?:;(.*)|)}/ ) {
+            my ( $dn, $opts ) = ( $1, $2 );
             chomp;
-            my @groupdn = split( /,/, $1 );
+            my @groupdn = split( /,/, $dn );
             my $groupname = shift(@groupdn);
             my ( $groupbase, $membersline );
             $groupbase = join( ',', @groupdn );
             $groupname =~ s/^cn=//;
+            my $attrs = $1 if $opts && $opts =~ /attr=(.*)/;
             push( @dirtyOUwatchlist, "$groupbase" );
-            ( my @memberlist ) = getMembers( $groupname, $groupbase );
+            ( my @memberlist ) = getMembers( $groupname, $groupbase, $attrs );
 
             # Fix up for -pmc
             if ( $groupdn[0] eq "ou=pmc" ) {
@@ -154,8 +156,10 @@ sub rebuild {
 sub getMembers {
     my $gid  = shift;
     my $base = shift;
+    my $attrs = shift || 'memberUid,member';
+
     my ( @members, @entries, $attr, @attrs );
-    print "getMembers: Looking for $gid, $base\n" if ($debug);
+    print "getMembers: Looking for $gid, $base, $attrs\n" if ($debug);
 
     # Look for memberUid for posix groups and member for groupOfNames
     # No group should ever have both?
@@ -164,7 +168,7 @@ sub getMembers {
         filter => "(cn=$gid)",
         base   => "$base",
         scope  => 'one',
-        attrs  => [ 'memberUid', 'member' ]
+        attrs  => [ split( /,/, $attrs ) ]
     );
     $request->code && die $request->error;
     @entries = $request->entries;
