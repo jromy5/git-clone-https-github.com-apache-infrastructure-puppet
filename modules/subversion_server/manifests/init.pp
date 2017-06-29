1218a5905473128234468514be3acb3c918e383b
Daniel Gruno <humbedooh@apache.org>
2017-06-29 11:05:17 +0200

    set user/group to exec as

--- modules/subversion_server/manifests/init.pp
+++ modules/subversion_server/manifests/init.pp
@@ -621,6 +621,8 @@ class subversion_server (
   # -w 10 == 10 workers, we can up that if need be.
   exec { 'cd /usr/lib/viewvc/cgi-bin/ && gunicorn -w 10 -b 127.0.0.1:8080 -d viewvc-wsgi:application':
     path   => '/usr/bin:/usr/sbin:/bin',
+    user    => 'www-data',
+    group   => 'www-data',
     onlyif => '! ps x | grep -q gunicorn',
   }
 
