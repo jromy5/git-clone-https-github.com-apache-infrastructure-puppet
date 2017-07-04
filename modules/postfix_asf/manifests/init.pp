5846f4793ffe5dc871aafd906bbdc5eb89d07bae
Daniel Takamori <pono@apache.org>
2017-07-04 20:00:56 +0000

    template for sender_access

--- modules/postfix_asf/manifests/init.pp
+++ modules/postfix_asf/manifests/init.pp
@@ -1,9 +1,17 @@
 #/etc/puppet/modules/postfix_asf/manifests/init.pp
 
 class postfix_asf (
+  $sender_access = '',
 ) {
 
   $dbfile = hiera_hash('postfix::dbfile', {})
   create_resources(postfix::dbfile, $dbfile)
 
+  File<|title == '/etc/postfix/sender_access'|> {
+      ensure => file,
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      content => template('postfix_asf/sender_access.erb'),
+    }
 }
