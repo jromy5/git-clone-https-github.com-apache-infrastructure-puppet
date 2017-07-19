5147e74f5c531d33d7d98f407a891f9a5e6eac4e
Daniel Takamori <pono@apache.org>
2017-07-19 15:57:19 +0000

    try to tag buildbot nodes with roles:builds

--- modules/buildbot_slave/manifests/init.pp
+++ modules/buildbot_slave/manifests/init.pp
@@ -25,6 +25,10 @@ class buildbot_slave (
 
   include buildbot_slave::buildbot
 
+  class { 'datadog_agent':
+    tags => ['roles:builds'],
+  }
+
   # install gradle PPA and gradle 2.x
 
   apt::ppa { 'ppa:cwchien/gradle':
