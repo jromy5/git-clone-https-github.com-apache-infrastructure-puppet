f9102258b9e8a08a71289cef9baf59f8ec829545
Chris Lambertus <cml@apache.org>
2017-08-14 15:32:07 -0700

    finish up pootle module and template files, add mysql database and move code from vagrant testing to live testing on new box

--- modules/pootle_asf/manifests/init.pp
+++ modules/pootle_asf/manifests/init.pp
@@ -1,44 +1,98 @@
 # module for installing custom asf pootle service (translate.a.o)
 
 class pootle_asf (
-  $po_root       = '/x1/www/po',
-  $repo_root     = '/x1/www/repos',
-  $pootle_dbname = 'pootle',        # eyaml
-  $pootle_dbuser = 'pootleuser',    # eyaml
-  $pootle_dbhost = '',              # eyaml
-  $pootle_dbpass = '',              # eyaml
+  $x1               = '/x1',
+  $pootle_dbname    = 'pootle',        # eyaml
+  $pootle_dbuser    = 'pootleuser',    # eyaml
+  $pootle_dbhost    = '',              # eyaml
+  $pootle_dbpass    = '',              # eyaml
+  $pootle_secretkey = '',              # eyaml
+  $nssbinddn        = '',
+  $nssbindpasswd    = '',
+
 ) {
 
-  file { $po_root:
+  # build some default paths
+
+  $pootle_wwwroot   = "${x1}/www"
+  $pootle_root      = "${pootle_wwwroot}/pootle"
+  $pootle_poroot    = "${pootle_wwwroot}/po"
+  $pootle_reporoot  = "${pootle_wwwroot}/repos"
+  $pootle_venv      = "${pootle_wwwroot}/pootle_venv"
+
+  file { $x1:
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  } ->
+
+  file { $pootle_wwwroot:
+    ensure  => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0775',
+    require => File['/x1'],
+  } ->
+
+  file { $pootle_root:
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0775',
+  } ->
+
+  file { $pootle_poroot:
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0775',
+  } ->
+
+  file { $pootle_reporoot:
     ensure => directory,
     owner  => 'www-data',
     group  => 'www-data',
     mode   => '0775',
   }
 
-  file { $repo_root:
-    ensure => directory,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0775',
-  }
-
-  file { '/x1/www/pootle/asf_db.py':
+  file { "${pootle_root}/wsgi.py":
     ensure  => present,
-    content => template('pootle_asf/asf_db.py.erb'),
+    content => template('pootle_asf/wsgi.py.erb'),
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0644',
   }
 
-  file { '/usr/local/lib/python2.7/dist-packages/pootle/settings.py':
+  file { "${pootle_root}/asf_db.py":
+    ensure  => present,
+    content => template('pootle_asf/asf_db.py.erb'),
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0644',
+  } ->
+
+  file { "${pootle_venv}/lib/python2.7/site-packages/pootle/asf_db.py":
     ensure => link,
-    target => '/x1/www/pootle/settings.py',
+    target => "${pootle_root}/asf_db.py",
   }
 
-  file { '/usr/local/lib/python2.7/dist-packages/pootle/asf_db.py':
+  file { "${pootle_root}/settings.py":
+    ensure  => present,
+    content => template('pootle_asf/settings.py.erb'),
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0644',
+  } ->
+
+  file { "${pootle_venv}/lib/python2.7/site-packages/pootle/settings.py":
     ensure => link,
-    target => '/x1/www/pootle/asf_db.py',
+    target => "${pootle_root}/settings.py",
+  }
+
+  file { "${pootle_root}/assets":
+    ensure => link,
+    target => "${pootle_venv}/lib/python2.7/site-packages/pootle/assets",
   }
 
 }
