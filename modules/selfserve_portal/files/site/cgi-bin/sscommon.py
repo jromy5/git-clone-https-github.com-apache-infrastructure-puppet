0d8967002a19cb4f931684530b52520c6c8244a9
Daniel Gruno <humbedooh@apache.org>
2017-08-23 21:33:40 +0200

    change users@ to private@ - less spam for ordinary folks

--- modules/selfserve_portal/files/site/cgi-bin/sscommon.py
+++ modules/selfserve_portal/files/site/cgi-bin/sscommon.py
@@ -33,10 +33,10 @@ def email(rcpt, subject, message):
     receivers = [rcpt]
     if isinstance(rcpt, list):
         receivers = rcpt
-    receivers.append('ASF Infrastructure <users@infra.apache.org>')
+    receivers.append('ASF Infrastructure <private@infra.apache.org>')
     msg = """From: %s
 To: %s
-Reply-To: ASF Infrastructure <users@infra.apache.org>
+Reply-To: ASF Infrastructure <private@infra.apache.org>
 Subject: %s
 
 %s
