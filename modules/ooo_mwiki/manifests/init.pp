17dc78ed2f326a2e1a89eb688737a658fa4d9937
gmcdonald <gmcdonald@apache.org>
2017-09-05 20:56:04 +1000

    Add imagemagick package

--- modules/ooo_mwiki/manifests/init.pp
+++ modules/ooo_mwiki/manifests/init.pp
@@ -10,7 +10,7 @@ class ooo_mwiki (
 
   # required packages
 
-  $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip'],
+  $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip' , 'imagemagick'],
 ){
 
 # install required packages:
