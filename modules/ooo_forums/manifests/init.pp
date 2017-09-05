3d062767caec4f0a72027a4ec9c7903e2ae46cbc
gmcdonald <gmcdonald@apache.org>
2017-09-05 14:04:44 +1000

    Flesh out beginnings of a phpBB forums module, start with some basic required PHP modules

---
+++ modules/ooo_forums/manifests/init.pp
@@ -0,0 +1,18 @@
+class ooo_forums (
+
+  # override below in yaml
+  $pbpbb_version = '',
+  $parent_dir,
+
+  # override below in eyaml
+
+  # required packages
+
+  $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip'],
+){
+
+# install required packages:
+  package {
+    $required_packages:
+      ensure => 'present',
+  }
