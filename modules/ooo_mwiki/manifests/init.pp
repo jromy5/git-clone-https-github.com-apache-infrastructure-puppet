26f5db1d00265c45f5eb770358efdd2b9faaab86
gmcdonald <gmcdonald@apache.org>
2017-09-05 21:03:31 +1000

    tweaks

--- modules/ooo_mwiki/manifests/init.pp
+++ modules/ooo_mwiki/manifests/init.pp
@@ -10,7 +10,7 @@ class ooo_mwiki (
 
   # required packages
 
-  $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip' , 'imagemagick'],
+  $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip' , 'imagemagick'], # lint:ignore:140chars
 ){
 
 # install required packages:
