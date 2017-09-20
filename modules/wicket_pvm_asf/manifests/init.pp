50905c3a94f4aafc6e9fc1f4dbadb5fdb101b951
gmcdonald <gmcdonald@apache.org>
2017-09-20 10:51:09 +1000

    Download/Install the wicket docker image from ASF Bintray

--- modules/wicket_pvm_asf/manifests/init.pp
+++ modules/wicket_pvm_asf/manifests/init.pp
@@ -4,6 +4,9 @@ class wicket_pvm_asf (
 
   $required_packages = ['tomcat8' , 'openjdk-8-jdk'],
 
+  # override below in yaml
+  $wicket_examples_version = '',
+
 ){
 
 # install required packages:
@@ -11,4 +14,12 @@ class wicket_pvm_asf (
     $required_packages:
       ensure => 'present',
   }
+
+# download wicket docker image from ASF Bintray instance
+  exec {
+    'download-wicket-docker':
+      command => "docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:${wicket_examples_version}",
+      timeout => 1200,
+  }
+
 }
