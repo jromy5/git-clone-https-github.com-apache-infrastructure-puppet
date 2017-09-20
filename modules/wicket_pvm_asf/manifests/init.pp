114bea6e6ca438cf8a6a8e4041d5fba1c75f8bbf
Daniel Takamori <pono@apache.org>
2017-09-20 08:53:48 -0400

    install docker before pulling container

--- modules/wicket_pvm_asf/manifests/init.pp
+++ modules/wicket_pvm_asf/manifests/init.pp
@@ -13,13 +13,14 @@ class wicket_pvm_asf (
   package {
     $required_packages:
       ensure => 'present',
-  }
+  }->
 
 # download wicket docker image from ASF Bintray instance
   exec {
     'download-wicket-docker':
       command => "docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:${wicket_examples_version}",
       timeout => 1200,
+      require => Package['docker-engine'],
   }
 
 }
