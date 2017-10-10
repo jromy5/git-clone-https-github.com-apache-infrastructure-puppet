052a750820481b63f5e99b043ee183c5e26f4770
gmcdonald <gmcdonald@apache.org>
2017-10-10 22:07:48 +1100

    Re-org. Add two more sets of examples images

--- modules/wicket_pvm_asf/manifests/init.pp
+++ modules/wicket_pvm_asf/manifests/init.pp
@@ -4,9 +4,6 @@ class wicket_pvm_asf (
 
   $required_packages = ['tomcat8' , 'openjdk-8-jdk', 'docker-engine'],
 
-  # override below in yaml
-  $wicket_examples_version = '',
-
 ){
 
 # install required packages:
@@ -15,17 +12,50 @@ class wicket_pvm_asf (
       ensure => 'present',
   }->
 
-# download wicket docker image from ASF Bintray instance
+# download wicket docker images from ASF Bintray instance - one for each version for demo.
+# for wicket-6
   exec {
-    'download-wicket-docker':
-      command => "/usr/bin/docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:${wicket_examples_version}",
+    'download-wicket-docker-6':
+      command => "/usr/bin/docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-6",
+      timeout => 1200,
+      require => Package['docker-engine'],
+  }
+# for wicket-7
+  exec {
+    'download-wicket-docker-7':
+      command => "/usr/bin/docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-7",
+      timeout => 1200,
+      require => Package['docker-engine'],
+  }
+# for wicket-8
+  exec {
+    'download-wicket-docker-8':
+      command => "/usr/bin/docker pull apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-8",
       timeout => 1200,
       require => Package['docker-engine'],
   }
 
+  docker::run { 'wicket-demo-6':
+    image           => 'apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-6',
+    ports           => ['8086', '8080'],
+    restart_service => true,
+    privileged      => false,
+    pull_on_start   => true,
+    extra_parameters => [ '--restart=always' ],
+  }
+
+  docker::run { 'wicket-demo-7':
+    image           => 'apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-7',
+    ports           => ['8087', '8080'],
+    restart_service => true,
+    privileged      => false,
+    pull_on_start   => true,
+    extra_parameters => [ '--restart=always' ],
+  }
+
   docker::run { 'wicket-demo-8':
-    image           => 'apache-docker-wicket-docker.bintray.io/wicket-examples',
-    ports           => ['8080', '8080'],
+    image           => 'apache-docker-wicket-docker.bintray.io/wicket-examples:LATEST-8',
+    ports           => ['8088', '8080'],
     restart_service => true,
     privileged      => false,
     pull_on_start   => true,
@@ -33,3 +63,4 @@ class wicket_pvm_asf (
   }
 
 }
+
