078a325d635182e8473bda761268f81963e0c3f1
gmcdonald <gmcdonald@apache.org>
2017-11-09 19:59:22 +1100

    Flehs out beginnings of mail_archives (eos replacement) module and initial yaml

---
+++ modules/mail_archives/manifests/init.pp
@@ -0,0 +1,42 @@
+#/etc/puppet/modules/mail_archives/manifests.init.pp
+
+class mail_archives (
+
+  $username   = 'modmbox',
+  $groupname  = 'modmbox',
+  $shell      = '/bin/bash',
+
+  # override below in yaml
+  $parent_dir,
+
+){
+
+  $install_dir = "${parent_dir}/mail-archives"
+
+  group {
+    $groupname:
+      ensure => $group_present,
+      system => true,
+  }->
+
+  user {
+    $username:
+      ensure  => $user_present,
+      system  => true,
+      name    => $username,
+      shell   => $shell,
+      gid     => $groupname,
+      require => Group[$groupname],
+  }
+
+  file {
+    $install_dir:
+      ensure => directory,
+      owner  => $username,
+      group  => 'root',
+    "${install_dir}/raw":
+      ensure => directory,
+      mode   => '0755';
+  }
+
+}
