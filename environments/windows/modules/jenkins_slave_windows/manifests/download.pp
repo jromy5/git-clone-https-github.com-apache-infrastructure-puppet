2bdd656bd88e9f79d28ca83df36eee7e62e9c871
Chris Thistlethwaite <christ@apache.org>
2017-11-22 17:32:07 -0500

    Added cygwin download and install. Moved download code to download.pp and left zip extraction in install.pp

--- environments/windows/modules/jenkins_slave_windows/manifests/download.pp
+++ environments/windows/modules/jenkins_slave_windows/manifests/download.pp
@@ -26,4 +26,62 @@ class jenkins_slave_windows::download (
     url                   => 'https://apache.bintray.com/WindowsPackages/asf-build-jdk9.0.exe',
     destination_directory => 'C:\temp',
   }
+
+  download_file { "Download cygwin from bintray" :
+    url                   => 'https://apache.bintray.com/WindowsPackages/cygwin64.zip',
+    destination_directory => 'C:\temp',
+  }
+
+  ###### download ant ######
+  define download_ant($ant_version = $title){
+    download_file { "Download asf-build-${ant_version} zip from bintray" :
+      url                   => "https://apache.bintray.com/WindowsPackages/asf-build-${ant_version}.zip",
+      destination_directory => 'F:\jenkins\tools\ant\zips',
+    }
+  }
+  define download_chromedriver($chromedriver_version = $title){
+      download_file { "Download asf-build-chromedriver-${chromedriver_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-chromedriver-${chromedriver_version}.zip",
+        destination_directory => 'F:\jenkins\tools\chromedriver\zips',
+      }
+    }
+  define download_geckodriver($geckodriver_version = $title){
+      download_file { "Download asf-build-geckodriver-${geckodriver_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-geckodriver-${geckodriver_version}.zip",
+        destination_directory => 'F:\jenkins\tools\geckodriver\zips',
+      }
+    }
+  define download_iedriver($iedriver_version = $title){
+      download_file { "Download asf-build-iedriver-${iedriver_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-iedriver-${iedriver_version}.zip",
+        destination_directory => 'F:\jenkins\tools\iedriver\zips',
+      }
+    }
+  define download_jdk($jdk_version = $title){
+      download_file { "Download asf-build-${jdk_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-${jdk_version}.zip",
+        destination_directory => 'F:\jenkins\tools\java\zips',
+      }
+    }
+  define download_maven($maven_version = $title){
+      download_file { "Download asf-build-${maven_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-${maven_version}.zip",
+        destination_directory => 'F:\jenkins\tools\maven\zips',
+      }
+    }
+  define download_nant($nant_version = $title){
+      download_file { "Download asf-build-nant-${nant_version} zip from bintray" :
+        url                   => "https://apache.bintray.com/WindowsPackages/asf-build-nant-${nant_version}.zip",
+        destination_directory => 'F:\jenkins\tools\nant\zips',
+      }
+    }
+
+  download_ant { $ant:}
+  download_chromedriver { $chromedriver:}
+  download_geckodriver { $geckodriver:}
+  download_iedriver { $iedriver:}
+  download_jdk { $jdk:}
+  download_maven { $maven:}
+  download_nant { $nant:}
+
 }
