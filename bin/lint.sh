213d69f7166185d6103f8c6116fad526fcde4999
Daniel Pono Takamori <pono@apache.org>
2017-12-12 15:35:30 -1000

    lint.sh should be most robust now

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export commit=$(git rev-parse HEAD)
-export files=$(git --no-pager show --pretty="" --name-only -r $commit | tr '\r\n' ' ')
-bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check $files
+export files=$(git --no-pager show --pretty="" --name-only -r $commit | grep -v .*sh|tr '\r\n' ' ')
+bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check modules/base/manifests/init.pp $files
