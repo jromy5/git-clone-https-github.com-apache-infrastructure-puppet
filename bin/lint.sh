8ff3f03f3f6f16a748e39174fc0fa78682943b1d
Daniel Pono Takamori <pono@apache.org>
2017-12-12 15:12:24 -1000

    arguments not set up correctly

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export commit=$(git rev-parse HEAD)
-export files=$(git --no-pager show --pretty="" --name-only| tr '\r\n' ' ')
-bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check -r $commit $files
+export files=$(git --no-pager show --pretty="" --name-only -r $commit | tr '\r\n' ' ')
+bundle exec puppet-lint --no-nested_classes_or_defines-check --no-autoloader_layout-check $files
