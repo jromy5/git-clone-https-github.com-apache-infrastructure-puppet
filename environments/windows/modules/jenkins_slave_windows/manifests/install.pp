4f21bde76abce171fa541c9001d3f47854b1b652
Chris Thistlethwaite <christ@apache.org>
2018-01-10 09:33:08 -0500

    Changed JRE to JDK

--- environments/windows/modules/jenkins_slave_windows/manifests/install.pp
+++ environments/windows/modules/jenkins_slave_windows/manifests/install.pp
@@ -28,7 +28,7 @@ class jenkins_slave_windows::install (
     #### Install JDK 1.8 silently for the system, but only if C:\Program Files\Java doesn't exist
   exec { 'install jdk' :
     command  => 'powershell.exe c:\temp\asf-build-jdk1.8.0_152.exe /s',
-    creates  => 'C:\Program Files\Java\jre1.8.0_152\bin\java.exe',
+    creates  => 'C:\Program Files\Java\jdk1.8.0_152\bin\java.exe',
     provider => powershell,
   }
 
