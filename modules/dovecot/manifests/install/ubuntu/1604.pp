f257883e7614edb1b018268d3cf09318c90720d7
Chris Lambertus <cml@apache.org>
2018-02-05 21:13:14 -0800

    we need a 1604 install manifest

---
+++ modules/dovecot/manifests/install/ubuntu/1604.pp
@@ -0,0 +1,17 @@
+#/etc/puppet/modules/dovecot/manifests/install/ubuntu/1604.pp
+
+class dovecot::install::ubuntu::1604 (
+  $ldapservers = hiera('ldapclient::ldapservers'),
+)
+{
+  file {
+    '/etc/dovecot/dovecot.conf':
+      content => template('dovecot/dovecot.conf.erb');
+    '/etc/dovecot/dovecot-ldap-userdb.conf.ext':
+      content => template('dovecot/dovecot-ldap-userdb.conf.ext.erb'),
+      require => File['/etc/dovecot/dovecot.conf'];
+    '/etc/dovecot/dovecot-ldap-passdb.conf.ext':
+      content => template('dovecot/dovecot-ldap-passdb.conf.ext.erb'),
+      require => File['/etc/dovecot/dovecot.conf'];
+  }
+}
