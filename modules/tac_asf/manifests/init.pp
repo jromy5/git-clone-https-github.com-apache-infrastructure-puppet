efae6c0502a187f5a56a0291411b048c00e9deaa
gmcdonald <gmcdonald@apache.org>
2018-02-07 13:25:45 +1100

    Add tac module

---
+++ modules/tac_asf/manifests/init.pp
@@ -0,0 +1,56 @@
+#/etc/puppet/modules/tac_asf/manifests/init.pp
+
+class tac_asf (
+
+  $username   = 'tac',
+  $groupname  = 'tac',
+
+  # override below in yaml
+
+$parent_dir,
+
+  # below are contained in eyaml
+
+){
+
+$install_dir    = "${parent_dir}/tac_app"
+$django_version = '1.3'
+
+  user {
+    $username:
+      ensure     => present,
+      system     => true,
+      name       => $username,
+      gid        => $groupname,
+      require    => Group[$groupname],
+  }
+
+  group {
+    $groupname:
+      ensure => present,
+      system => true,
+  }
+
+  file {
+    $parent_dir:
+      ensure => directory,
+      owner  => $username,
+      group  => $groupname,
+      mode   => '0755';
+    $install_dir:
+      ensure  => directory,
+      owner   => $username,
+      group   => $groupname,
+      require => File[$parent_dir];
+    "${parent_dir}/tac_wsgi.py":
+      owner   => $username,
+      group   => $groupname,
+      content => template('tac_asf/tac_wsgi.py.erb'),
+      mode    => '0755';
+    "${install_dir}/local_settings.py":
+      owner   => $username,
+      group   => $groupname,
+      content => template('tac_asf/local_settings.py.erb'),
+      mode    => '0750';
+  }
+}
