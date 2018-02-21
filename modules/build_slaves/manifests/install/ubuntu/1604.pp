cd7fd5bcf9065889ac2007a3b914051ecf661aac
gmcdonald <gmcdonald@apache.org>
2018-02-21 12:17:15 +1100

    ensure pid.max is set on 16.04 jenkins nodes. INFRA-15685

--- modules/build_slaves/manifests/install/ubuntu/1604.pp
+++ modules/build_slaves/manifests/install/ubuntu/1604.pp
@@ -1,6 +1,16 @@
 #/etc/puppet/modules/build_slaves/manifests/install/ubuntu/1604.pp
 
 class build_slaves::install::ubuntu::1604 (
+
+$jenkins_pids_max = $build_slaves::UserTasksMax
+
 ) {
 
+  file { 'pids.max':
+    ensure  => present,
+    path    => '/sys/fs/cgroup/pids/user.slice/user-910.slice/pids.max',
+    mode    => '0644',
+    content => $jenkins_pids_max,
+    require => [User[$build_slaves::username]];
+  }
 }
