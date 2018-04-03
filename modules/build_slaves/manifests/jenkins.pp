4fd69c89ab4033c51ae796c4c74c2e6e3e3dd533
Daniel Pono Takamori <pono@apache.org>
2018-04-03 08:44:16 -0600

    ensure absent for removing cron

--- modules/build_slaves/manifests/jenkins.pp
+++ modules/build_slaves/manifests/jenkins.pp
@@ -420,6 +420,12 @@ class build_slaves::jenkins (
 
 
   cron {
+    'docker-cleanup':
+      ensure      => absent,
+      hour        => '13',
+      command     => '/usr/bin/docker system prune -f',
+      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin\nSHELL=/bin/sh", # lint:ignore:double_quoted_strings
+  }
     'docker-cleanup-weekly':
       hour        => '20',
       command     => '/usr/bin/docker system prune -a -f -filter "until=168h"',
