a3c8803830aa3bee50762cd288514746e2fe31e4
Daniel Pono Takamori <pono@apache.org>
2018-04-03 08:46:39 -0600

    [OF] typos galore

--- modules/build_slaves/manifests/jenkins.pp
+++ modules/build_slaves/manifests/jenkins.pp
@@ -421,15 +421,15 @@ class build_slaves::jenkins (
 
   cron {
     'docker-cleanup':
-      ensure      => absent,
+      ensure      => 'absent',
       hour        => '13',
       command     => '/usr/bin/docker system prune -f',
-      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin\nSHELL=/bin/sh", # lint:ignore:double_quoted_strings
-  }
+      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin\nSHELL=/bin/sh"; # lint:ignore:double_quoted_strings
+
     'docker-cleanup-weekly':
       hour        => '20',
       command     => '/usr/bin/docker system prune -a -f -filter "until=168h"',
-      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin\nSHELL=/bin/sh", # lint:ignore:double_quoted_strings
+      environment => "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin\nSHELL=/bin/sh",; # lint:ignore:double_quoted_strings
   }
 
   cron {
