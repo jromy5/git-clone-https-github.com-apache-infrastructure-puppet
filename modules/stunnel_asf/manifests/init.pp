224b1a006cf59e79a18e67a66cc47f4d4f766903
Chris Lambertus <cml@apache.org>
2018-05-15 09:43:26 -0700

    mysql-rsync
    clean up more stunnel config/puppet parameters

--- modules/stunnel_asf/manifests/init.pp
+++ modules/stunnel_asf/manifests/init.pp
@@ -1,10 +1,13 @@
 #/etc/puppet/modules/stunnel_asf/manifests/init.pp
 
 class stunnel_asf (
-  $packages = ['stunnel4'],
-  $package_ensure = 'latest',
-  $stunnel_enable = true,
+  $packages        = ['stunnel4'],
+  $package_ensure  = 'latest',
+  $stunnel_enable  = true,
   $stunnel_options = '',
+  $stunnel_host    = 'abi.apache.org',
+  $stunnel_port    = '443',
+  $cert_path       = '/root/abi.apache.org.crt',
 ) {
 
   package { $packages:
@@ -19,12 +22,6 @@ class stunnel_asf (
       mode    => '0644',
       content => template('stunnel_asf/stunnel4.erb'),
       require => Package['stunnel4'];
-    'root_stunnel_conf':
-      path   => '/root/stunnel.conf',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/stunnel_asf/stunnel.conf';
     'root_abi_cert':
       path   => '/root/abi.apache.org.crt',
       owner  => 'root',
