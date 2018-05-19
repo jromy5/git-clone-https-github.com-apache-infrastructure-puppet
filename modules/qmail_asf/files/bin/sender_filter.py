ffd9b08dec36dd3f71df6eb6af5968f3bf263419
Sebb <sebb@apache.org>
2018-05-19 12:10:40 +0100

    Standardise python hash-bang

--- modules/qmail_asf/files/bin/sender_filter.py
+++ modules/qmail_asf/files/bin/sender_filter.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 #
 # Match ENV['SENDER'] against a list of email addresses (stored as a
 # simple list in a file). Should SENDER match, then exit(100).
