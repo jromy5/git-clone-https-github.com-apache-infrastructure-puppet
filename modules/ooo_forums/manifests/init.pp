9647abeba840dcf4464bafd95b26042e0584d6a5
Chris Lambertus <cml@apache.org>
2018-06-19 18:50:22 -0700

    OF: remove extraneous code

--- modules/ooo_forums/manifests/init.pp
+++ modules/ooo_forums/manifests/init.pp
@@ -5,12 +5,6 @@ class ooo_forums (
   $username      = 'phpbb',
   $groupname     = 'phpbb',
 
-  # override below in yaml
-  $parent_dir,
-  $pbpbb_version = '',
-
-  # override below in eyaml
-
   # required packages
 
   $required_packages = ['php7.0' , 'php7.0-curl' , 'php7.0-cli' , 'php7.0-json' , 'php7.0-mysql' , 'php7.0-xml' , 'php7.0-zip'],
